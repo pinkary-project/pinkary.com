fd9859cfc9a35653fb7cd21fba057bcb969d28fe
Mateus Junges <mateus@junges.dev>
2024-03-21 00:39:24 -0300

    Remove unused imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ namespace App\Models;
 
 use App\Services\Avatar;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
