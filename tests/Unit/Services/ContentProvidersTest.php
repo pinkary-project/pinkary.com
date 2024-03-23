1d94807b18ac98cf90e29e120ed9b7865a2b97c7
Javier Reyes <javier.reys@gmail.com>
2024-03-23 13:55:00 -0300

    test: extend the link provider parsable tests to check parsed mails

--- tests/Unit/Services/ContentProvidersTest.php
+++ tests/Unit/Services/ContentProvidersTest.php
@@ -40,6 +40,29 @@ test('links', function (string $content, string $parsed) {
     ],
 ]);
 
+test('links with mail', function (string $content, string $parsed) {
+    $provider = new App\Services\ParsableContentProviders\LinkProviderParsable();
+
+    expect($provider->parse($content))->toBe($parsed);
+})->with([
+    [
+        'content' => 'javier@example.com',
+        'parsed' => '<a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="mailto:javier@example.com">javier@example.com</a>',
+    ],
+    [
+        'content' => 'Hello my email is javier@example.com',
+        'parsed' => 'Hello my email is <a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="mailto:javier@example.com">javier@example.com</a>',
+    ],
+    [
+        'content' => 'Hello my email is javier@example.com, and my site is https://example.com',
+        'parsed' => 'Hello my email is <a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="mailto:javier@example.com">javier@example.com</a>, and my site is <a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="https://example.com">example.com</a>',
+    ],
+    [
+        'content' => 'Hello my emails are javier@example.com, contact@example.com and support@example.com.',
+        'parsed' => 'Hello my emails are <a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="mailto:javier@example.com">javier@example.com</a>, <a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="mailto:contact@example.com">contact@example.com</a> and <a class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="mailto:support@example.com">support@example.com</a>.',
+    ],
+]);
+
 test('code', function (string $content) {
     $provider = new App\Services\ParsableContentProviders\CodeProviderParsable();
 
