23c9bcf118e1eed7d69f27268c00175da18f66a4
Mateus Junges <mateus@junges.dev>
2024-03-24 03:12:36 -0300

    Add notifiation controller

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Notifications\DatabaseNotification;
+use Livewire\Features\SupportRedirects\Redirector;
+
+final class NotificationController
+{
+    public function update(User $user, DatabaseNotification $notification, Question $question): RedirectResponse|Redirector
+    {
+        $notification->markAsRead();
+
+        $question = Question::findOrFail($notification->data['question_id']);
+
+        return redirect()->route('questions.show', [
+            'user' => $user,
+            'question' => $question,
+        ]);
+    }
+}
