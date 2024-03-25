0204a2f19c5c4d3a9cb8c640df346a1a85aa82d6
Javier Reyes <javier.reys@gmail.com>
2024-03-25 12:06:35 -0300

    refactor: add padding for the home and notifications views on mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
     <x-slot name="title">Home</x-slot>
 
     <div class="flex flex-col items-center justify-center">
-        <div class="w-full max-w-md overflow-hidden rounded-lg shadow-md">
-            <livewire:home />
+        <div class="w-full max-w-md overflow-hidden rounded-lg shadow-md px-2 sm:px-0">
+            <livewire:home/>
         </div>
     </div>
 </x-app-layout>
