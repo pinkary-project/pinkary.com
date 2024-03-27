e23b56b83277108074d4650bdeaec950fc3122ba
Hosmel Quintana <hosmelq@gmail.com>
2024-03-26 21:19:57 -0600

    make home public

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Middleware\EnsureVerifiedEmailsForSignInUsers;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
+Route::view('/home', 'home')->name('home');
 Route::view('/terms', 'terms')->name('terms');
 Route::view('/privacy', 'privacy')->name('privacy');
 Route::view('/support', 'support')->name('support');
@@ -30,7 +31,6 @@ Route::prefix('/@{user:username}')->group(function () {
 });
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
-    Route::view('home', 'home')->name('home');
     Route::view('explore', 'explore')->name('explore');
 
     Route::get('notifications', [NotificationController::class, 'index'])->name('notifications.index');
