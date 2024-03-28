0d317c33dcb0efb1da1740124dd6c1636a86f206
Dasun Tharanga <hello@dasun.dev>
2024-03-28 20:55:07 +0530

    test: Improve the test with dataset

--- tests/Unit/Rules/NoBlankCharactersTest.php
+++ tests/Unit/Rules/NoBlankCharactersTest.php
@@ -4,22 +4,35 @@ declare(strict_types=1);
 
 use App\Rules\NoBlankCharacters;
 
-test('with blank characters', function () {
+test('with blank characters', function (string $name) {
     $rule = new NoBlankCharacters;
 
     $fail = fn (string $errorMessage) => $this->fail($errorMessage);
 
-    $rule->validate('name', '\u{200E}', $fail);
+    $rule->validate('name', $name, $fail);
 
     expect(true)->toBeFalse();
-})->fails();
+})->with([
+    "\u{200E}",
+    "\u{200E}\u{200E}",
+    "Test\u{200E}User",
+    "Test User \u{200E}",
+    "\u{200E}Test User",
+    "Test 1\u{200E}",
+    "测试\u{200E}",
+    "ⓣⓔⓢⓣ\u{200E}",
+    '  ',
+])->fails();
 
-test('without blank characters', function () {
+test('without blank characters', function (string $name) {
     $rule = new NoBlankCharacters;
 
     $fail = fn (string $errorMessage) => $this->fail($errorMessage);
 
-    $rule->validate('name', 'foo', $fail);
+    $rule->validate('name', $name, $fail);
 
     expect(true)->toBeTrue();
-});
+})->with([
+    'Nuno Maduro',
+    'Taylor Otwell',
+]);
