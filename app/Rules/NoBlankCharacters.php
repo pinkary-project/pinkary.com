23835bd9de3fbecdf2374a7a32f04346df4060d1
Dasun Tharanga <hello@dasun.dev>
2024-03-28 18:00:27 +0530

    feat: Create no blanks characters rule

---
+++ app/Rules/NoBlankCharacters.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+final class NoBlankCharacters implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (preg_match("/^(?=.*\u{200E}).*$/", $value)) {
+            $fail('The :attribute cannot contain invisible characters.');
+        }
+    }
+}
