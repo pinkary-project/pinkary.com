d08e0a3831bf08696869e31dfaff8a2bbe39ae62
Mateus Junges <mateus@junges.dev>
2024-03-29 01:12:20 -0300

    Add followers database table

---
+++ database/migrations/2024_03_29_040310_create_followers_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id')->constrained('users');
+            $table->foreignIdFor(User::class, 'follower_id')->constrained('users');
+            $table->unique(['user_id', 'follower_id']);
+        });
+    }
+};
