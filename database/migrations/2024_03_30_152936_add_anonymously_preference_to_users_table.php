6e36f61e99045d971dcfba37a5b2b4cd074b651e
Javier Reyes <javier.reys@gmail.com>
2024-03-30 13:02:32 -0300

    fix: set the default value of the anonymously_preference column to true

--- database/migrations/2024_03_30_152936_add_anonymously_preference_to_users_table.php
+++ database/migrations/2024_03_30_152936_add_anonymously_preference_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table): void {
-            $table->boolean('anonymously_preference')->default(false)->after('settings');
+            $table->boolean('anonymously_preference')->default(true)->after('settings');
         });
     }
 };
