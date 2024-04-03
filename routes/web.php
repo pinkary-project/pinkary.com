a45ad3ecf9656f482e88f3220844450e28a84de2
Nuno Guerra <nunoandre360@gmail.com>
2024-04-03 18:25:00 +0100

    chore: add changelog template

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::view('/terms', 'terms')->name('terms');
 Route::view('/privacy', 'privacy')->name('privacy');
 Route::view('/support', 'support')->name('support');
 Route::view('/brand/resources', 'brand.resources')->name('brand.resources');
+Route::view('/changelog', 'changelog')->name('changelog');
 
 Route::redirect('/sponsors', 'https://github.com/sponsors/nunomaduro/')->name('sponsors');
 
