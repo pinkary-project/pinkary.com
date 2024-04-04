d3df17ecf5ce739454cc5f39ef9510b070e9dd40
Nuno Guerra <nunoandre360@gmail.com>
2024-04-04 23:22:47 +0100

    chore: design tweaks

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -6,123 +6,119 @@
 
             <p class="text-slate-400">A changelog of the latest Pinkary feature releases, product updates and important bug fixes.</p>
 
-            <ul role="list" class="space-y-12 mt-16">
-                <li class="relative flex">
-                    <div class="absolute left-0 top-0 flex w-6 justify-center -bottom-14">
-                        <div class="w-px border-r border-dashed border-slate-700"></div>
-                    </div>
-                    <div class="relative flex size-6 flex-none items-center justify-center mr-4">
-                        <div class="size-1.5 rounded-full bg-pink-500 ring-1 ring-pink-500"></div>
-                    </div>
+            <div class="relative py-1 mt-12 mb-20">
+                <div class="absolute left-0 top-0 bottom-0 hidden sm:flex w-6 justify-center">
+                    <div class="w-px border-r border-dashed border-slate-700"></div>
+                </div>
 
-                    <div class="relative rounded-xl flex-1 overflow-hidden -mt-3 border border-slate-900">
-                        <div class="absolute -top-[200px] -right-[200px] transform-gpu blur-3xl z-10 opacity-20">
-                            <div class="size-[600px] bg-gradient-to-r from-pink-900 to-pink-500" style="clip-path:polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"></div>
-                        </div>
-                        <header class="flex-1 border-b border-slate-900 w-full px-4 py-3.5 flex items-center text-slate-200 justify-between">
-                            <h2 class="font-bold">Version 2.1</h2>
-                            <time datetime="2023-01-23T10:32" class="flex-none py-0.5 text-xs leading-5 text-slate-500 font-semibold">Mar 30, 2024</time>
-                        </header>
-                        <div class="prose prose-invert prose-sm prose-h3:text-sm px-4 py-3.5">
-                            <h3>Improvements & Changes</h3>
-                            <ul>
-                                <li>Add a new page with a changelog</li>
-                            </ul>
-
-                            <h3>Bugfixes</h3>
-                            <ul>
-                                <li>Fixed view / edit display links differently</li>
-                                <li>Fixed footer gets displayed weirdly when there is no content</li>
-                                <li>Fixed user was able to create questions and answers without content</li>
-                                <li>Fixed user was able to create an profile without name</li>
-                            </ul>
+                <ul role="list" class="space-y-6 sm:space-y-12">
+                    <li class="relative flex">
+                        <div class="relative size-6 flex-none items-center justify-center mr-4 mt-3.5 hidden sm:flex">
+                            <div class="size-1.5 rounded-full bg-pink-500 ring-1 ring-pink-500"></div>
                         </div>
 
-                        <div class="absolute inset-x-0 -bottom-2 flex h-2 justify-center overflow-hidden">
-                            <div class="-mt-px flex h-[2px] w-2/3 absolute right-5">
-                                <div class="w-full flex-none blur-sm bg-gradient-to-r from-slate-950 via-pink-400 to-slate-950"></div>
+                        <div class="relative rounded-xl flex-1 overflow-hidden border border-slate-900">
+                            <div class="absolute -top-[200px] -right-[200px] transform-gpu blur-3xl z-10 opacity-20">
+                                <div class="size-[600px] bg-gradient-to-r from-pink-900 to-pink-500" style="clip-path:polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"></div>
+                            </div>
+                            <header class="flex-1 border-b border-slate-900 w-full px-4 py-3.5 flex items-center text-slate-200 justify-between">
+                                <h2 class="font-bold">Version 2.1</h2>
+                                <time datetime="2023-01-23T10:32" class="flex-none py-0.5 text-xs leading-5 text-slate-500 font-semibold">Mar 30, 2024</time>
+                            </header>
+                            <div class="prose prose-invert prose-sm prose-h3:text-sm px-4 py-3.5">
+                                <h3>Improvements & Changes</h3>
+                                <ul>
+                                    <li>Add a new page with a changelog</li>
+                                </ul>
+
+                                <h3>Bugfixes</h3>
+                                <ul>
+                                    <li>Fixed view / edit display links differently</li>
+                                    <li>Fixed footer gets displayed weirdly when there is no content</li>
+                                    <li>Fixed user was able to create questions and answers without content</li>
+                                    <li>Fixed user was able to create an profile without name</li>
+                                </ul>
+                            </div>
+
+                            <div class="absolute inset-x-0 -bottom-2 flex h-2 justify-center overflow-hidden">
+                                <div class="-mt-px flex h-[2px] w-2/3 absolute right-5">
+                                    <div class="w-full flex-none blur-sm bg-gradient-to-r from-slate-950 via-pink-400 to-slate-950"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
+                    </li>
 
-                <li class="relative flex">
-                    <div class="absolute left-0 top-0 flex w-6 justify-center -bottom-14">
-                        <div class="w-px border-r border-dashed border-slate-700"></div>
-                    </div>
-                    <div class="relative flex size-6 flex-none items-center justify-center mr-4">
-                        <div class="size-1.5 rounded-full bg-pink-500 ring-1 ring-pink-500"></div>
-                    </div>
-
-                    <div class="relative rounded-xl flex-1 overflow-hidden -mt-3 border border-slate-900">
-                        <div class="absolute -top-[200px] -right-[200px] transform-gpu blur-3xl z-10 opacity-20">
-                            <div class="size-[600px] bg-gradient-to-r from-pink-900 to-pink-500" style="clip-path:polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"></div>
-                        </div>
-                        <header class="flex-1 border-b border-slate-900 w-full px-4 py-3.5 flex items-center text-slate-200 justify-between">
-                            <h2 class="font-bold">Version 2.0</h2>
-                            <time datetime="2023-01-23T10:32" class="flex-none py-0.5 text-xs leading-5 text-slate-500 font-semibold">Mar 30, 2024</time>
-                        </header>
-                        <div class="prose prose-invert prose-sm prose-h3:text-sm px-4 py-3.5">
-                            <h3>Improvements & Changes</h3>
-                            <ul>
-                                <li>Add a new page with a changelog</li>
-                                <li>Add a follow button</li>
-                            </ul>
-
-                            <h3>Bugfixes</h3>
-                            <ul>
-                                <li>Fixed footer gets displayed weirdly when there is no content</li>
-                                <li>Fixed user was able to create an profile without name</li>
-                            </ul>
+                    <li class="relative flex">
+                        <div class="relative size-6 flex-none items-center justify-center mr-4 mt-3.5 hidden sm:flex">
+                            <div class="size-1.5 rounded-full bg-pink-500 ring-1 ring-pink-500"></div>
                         </div>
 
-                        <div class="absolute inset-x-0 -bottom-2 flex h-2 justify-center overflow-hidden">
-                            <div class="-mt-px flex h-[2px] w-2/3 absolute right-5">
-                                <div class="w-full flex-none blur-sm bg-gradient-to-r from-slate-950 via-pink-400 to-slate-950"></div>
+                        <div class="relative rounded-xl flex-1 overflow-hidden border border-slate-900">
+                            <div class="absolute -top-[200px] -right-[200px] transform-gpu blur-3xl z-10 opacity-20">
+                                <div class="size-[600px] bg-gradient-to-r from-pink-900 to-pink-500" style="clip-path:polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"></div>
+                            </div>
+                            <header class="flex-1 border-b border-slate-900 w-full px-4 py-3.5 flex items-center text-slate-200 justify-between">
+                                <h2 class="font-bold">Version 2.0</h2>
+                                <time datetime="2023-01-23T10:32" class="flex-none py-0.5 text-xs leading-5 text-slate-500 font-semibold">Mar 30, 2024</time>
+                            </header>
+                            <div class="prose prose-invert prose-sm prose-h3:text-sm px-4 py-3.5">
+                                <h3>Improvements & Changes</h3>
+                                <ul>
+                                    <li>Add a new page with a changelog</li>
+                                    <li>Add a follow button</li>
+                                </ul>
+
+                                <h3>Bugfixes</h3>
+                                <ul>
+                                    <li>Fixed footer gets displayed weirdly when there is no content</li>
+                                    <li>Fixed user was able to create an profile without name</li>
+                                </ul>
+                            </div>
+
+                            <div class="absolute inset-x-0 -bottom-2 flex h-2 justify-center overflow-hidden">
+                                <div class="-mt-px flex h-[2px] w-2/3 absolute right-5">
+                                    <div class="w-full flex-none blur-sm bg-gradient-to-r from-slate-950 via-pink-400 to-slate-950"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
+                    </li>
 
-                <li class="relative flex">
-                    <div class="absolute left-0 top-0 flex w-6 justify-center -bottom-6">
-                        <div class="w-px border-r border-dashed border-slate-700"></div>
-                    </div>
-                    <div class="relative flex size-6 flex-none items-center justify-center mr-4">
-                        <div class="h-1.5 w-1.5 rounded-full bg-pink-500 ring-1 ring-pink-500"></div>
-                    </div>
-
-                    <div class="relative rounded-xl flex-1 overflow-hidden -mt-3 border border-slate-900">
-                        <div class="absolute -top-[200px] -right-[200px] transform-gpu blur-3xl -z-10 opacity-20">
-                            <div class="size-[600px] bg-gradient-to-r from-pink-900 to-pink-500" style="clip-path:polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"></div>
-                        </div>
-                        <header class="flex-1 border-b border-black/80 w-full px-4 py-3.5 flex items-center text-slate-200 justify-between">
-                            <h2 class="font-bold">Version 1.2</h2>
-                            <time datetime="2023-01-23T10:32" class="flex-none py-0.5 text-xs leading-5 text-slate-500 font-semibold">Mar 23, 2024</time>
-                        </header>
-                        <div class="prose prose-invert prose-sm prose-h3:text-sm px-4 py-3.5">
-                            <h3>Improvements & Changes</h3>
-                            <ul>
-                                <li>Add a new page with a changelog</li>
-                                <li>Add a follow button</li>
-                            </ul>
-
-                            <h3>Bugfixes</h3>
-                            <ul>
-                                <li>Fixed footer gets displayed weirdly when there is no content</li>
-                                <li>Fixed user was able to create questions and answers without content</li>
-                                <li>Fixed user was able to create an profile without name</li>
-                            </ul>
+                    <li class="relative flex">
+                        <div class="relative size-6 flex-none items-center justify-center mr-4 mt-3.5 hidden sm:flex">
+                            <div class="size-1.5 rounded-full bg-pink-500 ring-1 ring-pink-500"></div>
                         </div>
 
-                        <div class="absolute inset-x-0 -bottom-2 flex h-2 justify-center overflow-hidden">
-                            <div class="-mt-px flex h-[2px] w-2/3 absolute right-5">
-                                <div class="w-full flex-none blur-sm bg-gradient-to-r from-slate-950 via-pink-400 to-slate-950"></div>
+                        <div class="relative rounded-xl flex-1 overflow-hidden border border-slate-900">
+                            <div class="absolute -top-[200px] -right-[200px] transform-gpu blur-3xl z-10 opacity-20">
+                                <div class="size-[600px] bg-gradient-to-r from-pink-900 to-pink-500" style="clip-path:polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"></div>
+                            </div>
+                            <header class="flex-1 border-b border-slate-900 w-full px-4 py-3.5 flex items-center text-slate-200 justify-between">
+                                <h2 class="font-bold">Version 2.0</h2>
+                                <time datetime="2023-01-23T10:32" class="flex-none py-0.5 text-xs leading-5 text-slate-500 font-semibold">Mar 30, 2024</time>
+                            </header>
+                            <div class="prose prose-invert prose-sm prose-h3:text-sm px-4 py-3.5">
+                                <h3>Improvements & Changes</h3>
+                                <ul>
+                                    <li>Add a new page with a changelog</li>
+                                    <li>Add a follow button</li>
+                                </ul>
+
+                                <h3>Bugfixes</h3>
+                                <ul>
+                                    <li>Fixed footer gets displayed weirdly when there is no content</li>
+                                    <li>Fixed user was able to create an profile without name</li>
+                                </ul>
+                            </div>
+
+                            <div class="absolute inset-x-0 -bottom-2 flex h-2 justify-center overflow-hidden">
+                                <div class="-mt-px flex h-[2px] w-2/3 absolute right-5">
+                                    <div class="w-full flex-none blur-sm bg-gradient-to-r from-slate-950 via-pink-400 to-slate-950"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 
