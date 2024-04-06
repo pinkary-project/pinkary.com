f8a5c10aa842114027253ec1132607213ae2d4f5
Nuno Maduro <enunomaduro@gmail.com>
2024-04-06 16:21:32 +0100

    feat(click-count): minor ui change

--- resources/views/livewire/links/index.blade.php
+++ resources/views/livewire/links/index.blade.php
@@ -105,10 +105,7 @@
 
                             <div class="flex items-center justify-center">
                                 <div class="hidden group-hover:flex min-w-fit items-center gap-1 text-xs" title="Clicked {{ $link->click_count }} times">
-                                    {{ $link->click_count }}
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
-                                    </svg>
+                                    <span >{{ $link->click_count }} {{ str('click')->plural($link->click_count) }}</span>
                                 </div>
                                 <form wire:submit="destroy({{ $link->id }})">
                                     <button
@@ -125,7 +122,6 @@
                 </ul>
             @else
                 <div class="space-y-3">
-                    {{-- Just listing links --}}
                     @foreach ($links as $link)
                         <div
                             class="{{ $user->link_shape }} {{ $user->gradient }} hover:darken-gradient flex bg-gradient-to-r"
