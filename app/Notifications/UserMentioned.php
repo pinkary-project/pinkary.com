865ac1fa2ff2ce557a5f8271d430f4f86487425c
Francisco José Barba López <franciscojose.barbalopez@gmail.com>
2024-04-06 13:07:44 +0200

    created notification

---
+++ app/Notifications/UserMentioned.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserMentioned extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public Question $question,
+        public User $user
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'question_id' => $this->question->id,
+            'user_id' => $this->user->id,
+        ];
+    }
+}
