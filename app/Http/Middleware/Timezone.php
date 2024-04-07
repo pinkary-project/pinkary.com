93083960c86266b457b1dbfdddb9d3ad5087c670
Tomás López <tomloprod@gmail.com>
2024-04-07 17:24:57 +0200

    feat: timezone assignment middleware

---
+++ app/Http/Middleware/Timezone.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final readonly class Timezone
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        /** @var string $timezone */
+        $timezone = session()->get('timezone', 'UTC');
+
+        date_default_timezone_set($timezone);
+
+        Carbon::setLocale($timezone);
+
+        config(['app.timezone' => $timezone]);
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\Timezone;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->appendToGroup('web', Timezone::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
