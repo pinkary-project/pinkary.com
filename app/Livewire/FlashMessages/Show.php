8905abfe466d72de1a7670989ec174824bdaea9a
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-07 17:39:14 +0530

    Refactor flash message handling in Show component

--- app/Livewire/FlashMessages/Show.php
+++ app/Livewire/FlashMessages/Show.php
@@ -11,25 +11,6 @@ use Livewire\Component;
 final class Show extends Component
 {
     /**
-     * Flashes a new message.
-     */
-    #[On('notification.created')]
-    public function flash(string $message): void
-    {
-        session()->flash('flash-message', $message);
-
-        $this->js('setTimeout(() => { $wire.flush() }, 3000)');
-    }
-
-    /**
-     * Flushes the flash message.
-     */
-    public function flush(): void
-    {
-        session()->forget('flash-message');
-    }
-
-    /**
      * Render the component.
      */
     public function render(): View
