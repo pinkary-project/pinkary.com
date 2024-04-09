55b756d3e4131b304aa83df5b27dcf722714f05b
Adam Lee <adam@codelabx.ltd>
2024-04-09 23:36:38 +0100

    fix: Allow to use mysql without breaking changes

--- database/migrations/2024_02_25_144346_create_likes_table.php
+++ database/migrations/2024_02_25_144346_create_likes_table.php
@@ -6,6 +6,7 @@ use App\Models\Question;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,7 +19,10 @@ return new class extends Migration
         Schema::create('likes', function (Blueprint $table): void {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
+
+            DB::connection()->getDriverName() === 'mysql'
+                ? $table->foreignIdFor(Question::class)
+                : $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
 
             $table->unique(['user_id', 'question_id']);
 
