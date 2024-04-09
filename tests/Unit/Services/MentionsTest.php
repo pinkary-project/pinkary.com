ad65dc0ed38e5183d79277b64aa21ccf20a40d85
Francisco José Barba López <franciscojose.barbalopez@gmail.com>
2024-04-09 15:55:25 +0200

    adding test for only valid mentions retrieved

--- tests/Unit/Services/MentionsTest.php
+++ tests/Unit/Services/MentionsTest.php
@@ -35,3 +35,17 @@ test('mentions are unique', function () {
     expect($mentions->count())->toBe(1);
     expect($mentions->first()->username)->toBe('johndoe');
 });
+
+test('only valid user mentions are retrieved', function () {
+    User::factory()->create(['username' => 'johndoe']);
+
+    $question = Question::factory()->create([
+        'content' => 'Hello @johndoe! How are you doing?',
+        'answer' => 'I am doing great, @invalid!',
+    ]);
+
+    $mentions = (new Mentions())->usersMentioned($question);
+
+    expect($mentions->count())->toBe(1);
+    expect($mentions->first()->username)->toBe('johndoe');
+});
