684c8662aa8c28a74693e5a6a134b941e47824ac
Adam Lee <adam@codelabx.ltd>
2024-04-10 23:55:59 +0100

    chore: Updated arch tests

--- tests/Arch.php
+++ tests/Arch.php
@@ -2,135 +2,135 @@
 
 declare(strict_types=1);
 
-arch('Expect application to be using strict types')
+arch('strict types')
     ->expect('App')
     ->toUseStrictTypes();
 
-arch('Expect the following functions to not be used')
+arch('functions')
     ->expect(['dd', 'dump', 'die', 'var_dump'])
     ->not->toBeUsed();
 
-arch('Expect the following helpers only to be used in specific namespaces')
+arch('helpers')
     ->expect(['session', 'auth', 'request'])
     ->toOnlyBeUsedIn([
         'App\Http',
         'App\Livewire',
     ]);
 
-arch('Expect all console commands to extend Command')
+arch('commands')
     ->expect('App\Console\Commands')
     ->toExtend('Illuminate\Console\Command')
     ->toHaveMethod('handle')
     ->toBeFinal();
 
-arch('Expect all contracts to be interfaces')
+arch('contracts')
     ->expect('App\Contracts')
     ->toBeInterfaces();
 
-arch('Expect all controllers to be final')
+arch('controllers')
     ->expect('App\Http\Controllers')
     ->toHaveSuffix('Controller')
     ->ignoring('App\Http\Controllers\Auth')
     ->classes()
     ->toBeFinal();
 
-arch('Expect all middeleware to have handle method')
+arch('middleware')
     ->expect('App\Http\Middleware')
     ->classes()
     ->toBeFinal()
     ->toHaveMethod('handle');
 
-arch('Expect all jobs to implement ShouldQueue')
+arch('jobs')
     ->expect('App\Jobs')
     ->toHaveConstructor()
     ->toHaveMethod('handle')
     ->toImplement('Illuminate\Contracts\Queue\ShouldQueue');
 
-arch('Expect all livewire components to extend Component')
+arch('livewire components')
     ->expect('App\Livewire')
     ->toExtend('Livewire\Component')
     ->classes()
     ->toBeFinal();
 
-arch('Expect all mail classes to extend Mailable')
+arch('mailables')
     ->expect('App\Mail')
     ->toHaveConstructor()
-    // ->toHaveTraits([
-    //     'Illuminate\Bus\Queueable',
-    //     'Illuminate\Queue\SerializesModels',
-    // ])
     ->toExtend('Illuminate\Mail\Mailable');
 
-arch('Expect all models to be classes')
+arch('models')
     ->expect('App\Models')
     ->toHaveMethod('casts')
     ->toExtend('Illuminate\Database\Eloquent\Model')
-    //->toHaveTrait('Illuminate\Database\Eloquent\Factories\HasFactory')
     ->toBeClasses()
     ->toBeFinal();
 
-arch('Expect all notifications to extend Notification')
+arch('notifications')
     ->expect('App\Notifications')
     ->toHaveConstructor()
-    // ->toHaveTrait('Illuminate\Bus\Queueable)
     ->toExtend('Illuminate\Notifications\Notification');
 
-arch('Expect all observers to be readonly classes')
+arch('observers')
     ->expect('App\Observers')
     ->toBeFinal()
     ->toBeReadonly()
     ->toExtendNothing();
 
-arch('Expect all policies to be readonly classes')
+arch('policies')
     ->expect('App\Policies')
     ->toBeFinal()
     ->toBeReadonly()
     ->toExtendNothing();
 
-arch('Expect all providers to extend ServiceProvider')
+arch('providers')
     ->expect('App\Providers')
     ->toExtend('Illuminate\Support\ServiceProvider')
     ->toBeFinal();
 
-arch('Expect all rules to implement ValidationRule')
+arch('rules')
     ->expect('App\Rules')
     ->toImplement('Illuminate\Contracts\Validation\ValidationRule')
     ->toBeFinal()
     ->toBeReadonly()
     ->toExtendNothing();
 
-arch('Expect all service classes to implement something')
+arch('services')
     ->expect('App\Services')
     ->toBeFinal()
     ->toBeReadonly()
     ->toExtendNothing();
 
-arch('Expect all parsable content providers to implement ParsableContentProvider')
+arch('parsable content providers')
     ->expect('App\Services\ParsableContentProviders')
     ->toImplement('App\Contracts\ParsableContentProvider')
     ->toBeFinal()
     ->toBeReadonly()
     ->toExtendNothing();
 
-arch('Expect all view component classes to extend Component')
+arch('view components')
     ->expect('App\View\Components')
     ->toExtend('Illuminate\View\Component')
     ->toHaveMethod('render')
     ->toBeFinal();
 
-// Suggestions to improve arc-rules add more expectations to cover types and unless extends
+arch('avoid extends')
+    ->expect('App')
+    ->classes()
+    ->toBeFinal();
 
-// arch('Expect all classes to be readonly unless they extend something')
-//     ->expect('App')
-//     ->classes()
-//     ->toBeReadonly()
-//     ->unlessExtendsSomething();
-
-// arch('All classes should always declare return types')
-//     ->expect('App')
-//     ->toReturnType();
-
-// arch('Controller methods edit show and index should return a view type')
-//     ->expect('App\Http\Controllers')
-//     ->toHaveMethod(['edit', 'show', 'index'])
-//     ->toReturnType('Illuminate\View\View');
+arch('avoid mutation')
+    ->expect('App')
+    ->classes()
+    //->toBeReadonlyUnlessExtends()
+    ->toBeReadonly()
+    ->ignoring([
+        'App\Console\Commands',
+        'App\Exceptions',
+        'App\Http\Controllers\Auth\Requests',
+        'App\Jobs',
+        'App\Livewire',
+        'App\Mail',
+        'App\Models',
+        'App\Notifications',
+        'App\Providers',
+        'App\View',
+    ]);
