dae9441666e1eeefafaad36ca5a1e25b27ce5051
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-12 19:07:11 +0530

    Add UserMailPreference::toArray() method

--- app/Enums/UserMailPreference.php
+++ app/Enums/UserMailPreference.php
@@ -8,4 +8,14 @@ enum UserMailPreference: string
 {
     case Daily = 'daily';
     case Weekly = 'weekly';
+    case Never = 'never';
+
+    public static function toArray(): array
+    {
+        return [
+            self::Daily->value => 'Daily',
+            self::Weekly->value => 'Weekly',
+            self::Never->value => 'Never',
+        ];
+    }
 }
