1f13e3f84df0c3a98c4bcc36c69dd56ea8ed548f
Adam Lee <adam@codelabx.ltd>
2024-04-14 00:36:27 +0100

    chore: Cleanup contracts test

--- tests/Arch/ContractsTest.php
+++ tests/Arch/ContractsTest.php
@@ -5,16 +5,4 @@ declare(strict_types=1);
 arch('contracts')
     ->expect('App\Contracts')
     ->interfaces()
-    //->toHaveAtLeastOneMethod()
     ->toBeInterfaces();
-
-// arch('implement methods')
-//     ->expect('App')
-//     ->classes()
-//     ->toHaveImplementedMethods();
-
-/**
- * New Expectations:
- * toHaveAtLeastOneMethod
- * toHaveImplementedMethods
- */
