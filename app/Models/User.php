819daf10282fc794aad744038c819c8cb7bd128b
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-13 08:05:53 +0530

    Fix case sensitivity issue in User model resolveRouteBinding method

--- app/Models/User.php
+++ app/Models/User.php
@@ -233,7 +233,7 @@ final class User extends Authenticatable implements MustVerifyEmail
     public function resolveRouteBinding($value, $field = null)
     {
         return match ($field) {
-            'username' => $this->where(DB::raw('LOWER(username)'), mb_strtolower($value))->firstOrFail(),
+            'username' => $this->where(DB::raw('LOWER(username)'), mb_strtolower((string) $value))->firstOrFail(),
             default => parent::resolveRouteBinding($value, $field),
         };
     }
