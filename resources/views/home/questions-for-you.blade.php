ef35b84f3c13d028ff788b87f933a9e4177b54ab
Tomás López <tomloprod@gmail.com>
2024-04-13 16:12:49 +0200

    feat(ForYou): Apply a margin to ensure the section looks correct when there's no authenticated user

--- resources/views/home/questions-for-you.blade.php
+++ resources/views/home/questions-for-you.blade.php
@@ -8,14 +8,16 @@
             @auth
                 <livewire:home.questions-for-you :focus-input="true" />
             @else
-                <div class="mb-4">Log in or sign up to access personalized content.</div>
+                <div class="mb-4">
+                    <div class="mb-4">Log in or sign up to access personalized content.</div>
 
-                <a href="{{ route('login') }}" wire:navigate>
-                    <x-primary-button>Log In</x-primary-button>
-                </a>
-                <a href="{{ route('register') }}" wire:navigate>
-                    <x-primary-button>Register</x-primary-button>
-                </a>
+                    <a href="{{ route('login') }}" wire:navigate>
+                        <x-primary-button>Log In</x-primary-button>
+                    </a>
+                    <a href="{{ route('register') }}" wire:navigate>
+                        <x-primary-button>Register</x-primary-button>
+                    </a>
+                <div>
             @endauth
         </div>
     </div>
