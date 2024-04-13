afce8f742d17e13981c59d18724e77d14a7c9c95
Cameron Kemshal-Bell <camkb@icloud.com>
2024-04-13 22:52:29 +1000

    refactor: updated tests to assert the job was pushed

--- tests/Http/ExploreTest.php
+++ tests/Http/ExploreTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Jobs\IncrementViews;
 use App\Livewire\Explore\QuestionsForYou;
 use App\Livewire\Explore\TrendingQuestions;
 use App\Livewire\Users\Index;
@@ -16,14 +17,17 @@ it('can see the explore "users" view', function () {
 });
 
 it('can see the explore "trending" view', function () {
+    Queue::fake(IncrementViews::class);
     $response = $this->get(route('explore.trending'));
 
     $response->assertOk()
         ->assertSee('Trending')
         ->assertSeeLivewire(TrendingQuestions::class);
+    Queue::assertPushed(IncrementViews::class);
 });
 
 it('can see the explore "for you" view', function () {
+    Queue::fake(IncrementViews::class);
     $user = User::factory()->create();
 
     $response = $this->actingAs($user)->get(route('explore.for_you'));
@@ -31,11 +35,14 @@ it('can see the explore "for you" view', function () {
     $response->assertOk()
         ->assertSee('For you')
         ->assertSeeLivewire(QuestionsForYou::class);
+    Queue::assertPushed(IncrementViews::class);
 });
 
 it('guest can see the explore "for you" view', function () {
+    Queue::fake(IncrementViews::class);
     $response = $this->get(route('explore.for_you'));
 
     $response->assertOk()
         ->assertSee('Log in or sign up to access personalized content');
+    Queue::assertNotPushed(IncrementViews::class);
 });
--- tests/Http/FeedTest.php
+++ tests/Http/FeedTest.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
+use App\Jobs\IncrementViews;
 use App\Livewire\Feed;
 
 it('can see the home view', function () {
+    Queue::fake(IncrementViews::class);
     $response = $this->get(route('feed'));
 
     $response->assertOk()
         ->assertSee('Home')
         ->assertSeeLivewire(Feed::class);
+
+    Queue::assertPushed(IncrementViews::class);
 });
