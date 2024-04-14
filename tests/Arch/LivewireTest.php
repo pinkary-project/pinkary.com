ab0e8ccae832d0e4216c3605a4ccf87c2e7af98e
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-14 18:10:24 +0530

    Refactor LivewireTest.php to ignore App\Livewire\Concerns and add arch for livewire concerns

--- tests/Arch/LivewireTest.php
+++ tests/Arch/LivewireTest.php
@@ -5,8 +5,21 @@ declare(strict_types=1);
 arch('livewire components')
     ->expect('App\Livewire')
     ->toExtend('Livewire\Component')
+    ->ignoring([
+        'App\Livewire\Concerns',
+    ])
     ->toHaveMethod('render')
+    ->ignoring([
+        'App\Livewire\Concerns',
+    ])
     ->toOnlyBeUsedIn([
         'App\Http\Controllers',
         'App\Http\Livewire',
+    ])
+    ->ignoring([
+        'App\Livewire\Concerns',
     ]);
+
+arch('livewire concerns')
+    ->expect('App\Livewire\Concerns')
+    ->toBeTraits();
