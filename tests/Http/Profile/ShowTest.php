289368cc1a2e2e1d8133613466bcf55f7c3100d5
Cameron Kemshal-Bell <camkb@icloud.com>
2024-04-14 11:07:45 +1000

    refactor: ran pint & type test

--- tests/Http/Profile/ShowTest.php
+++ tests/Http/Profile/ShowTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use App\Jobs\IncrementViews;
 use App\Models\User;
-use Illuminate\Queue\Jobs\Job;
 
 beforeEach(function () {
     $this->user = User::factory()->create();
