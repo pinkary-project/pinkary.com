2bfc0030630ac00d61977b01b94fb06d425a1003
Adam Lee <adam@codelabx.ltd>
2024-04-16 00:28:25 +0100

    fix: revert index view

--- resources/views/livewire/questions/index.blade.php
+++ resources/views/livewire/questions/index.blade.php
@@ -1,7 +1,8 @@
 <section class="mt-4 space-y-10">
-    @foreach ($this->questions as $question)
+    @foreach ($questions as $question)
         <livewire:questions.show :questionId="$question->id" :key="'question-' . $question->id" :inIndex="true" :pinnable="$pinnable" />
     @endforeach
 
-{{--    <x-load-more-button :perPage="$perPage" message="There are no more questions to load, or you have scrolled too far." />--}}
+    <x-load-more-button :perPage="$perPage" :paginator="$questions"
+        message="There are no more questions to load, or you have scrolled too far." />
 </section>
