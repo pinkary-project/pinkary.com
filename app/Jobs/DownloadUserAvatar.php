6684a0b07275c088decaf1d58049f61e574d43ba
Cameron Kemshal-Bell <camkb@icloud.com>
2024-04-18 17:29:40 +1000

    feat(upload-avatar): extract common logic to action classes

--- app/Jobs/DownloadUserAvatar.php
+++ app/Jobs/DownloadUserAvatar.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Jobs;
 
+use App\Actions\Profile\DeleteAvatar;
+use App\Actions\Profile\StoreAvatar;
 use App\Models\User;
 use App\Services\Avatar;
 use Illuminate\Bus\Queueable;
@@ -11,7 +13,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Storage;
 
 final class DownloadUserAvatar implements ShouldQueue
 {
@@ -32,47 +33,27 @@ final class DownloadUserAvatar implements ShouldQueue
     {
         $avatar = $this->fetchNewAvatar();
 
-        $this->deleteExistingAvatar();
+        if ($this->user->avatar) {
+            DeleteAvatar::execute($this->user->avatar);
+        }
+
+        $location = StoreAvatar::execute($avatar->url(), $this->user->id);
 
         $this->user->update([
-            'avatar' => 'storage/'.$avatar,
+            'avatar' => 'storage/'.$location,
             'avatar_updated_at' => now(),
+            'has_custom_avatar' => false,
         ]);
     }
 
     /**
      * Fetch a new avatar for the user.
      */
-    private function fetchNewAvatar(): string
+    private function fetchNewAvatar(): Avatar
     {
         /** @var array<int, string> $urls */
         $urls = $this->user->links->pluck('url')->values()->all();
 
-        $avatar = new Avatar($this->user->email, $urls);
-        $contents = app()->environment('testing') ? '...' : (string) file_get_contents($avatar->url());
-
-        $avatar = 'avatars/'.hash('sha256', random_int(0, PHP_INT_MAX).'@'.$this->user->id).'.png';
-
-        Storage::disk('public')->put($avatar, $contents, 'public');
-
-        return $avatar;
-    }
-
-    /**
-     * Delete the current avatar.
-     */
-    private function deleteExistingAvatar(): void
-    {
-        $avatar = $this->user->avatar;
-
-        if (! $avatar) {
-            return;
-        }
-
-        if (! Storage::disk('public')->exists(str_replace('storage/', '', $avatar))) {
-            return;
-        }
-
-        Storage::disk('public')->delete(str_replace('storage/', '', $avatar));
+        return new Avatar($this->user->email, $urls);
     }
 }
