4bc67e0558cb97cfe493eca0e23128999f8d7910
Stephen Rees-Carter <stephen@rees-carter.net>
2024-04-19 14:54:51 +1000

    Add rel="me noopener" to profile links

--- resources/views/components/links/list-item.blade.php
+++ resources/views/components/links/list-item.blade.php
@@ -10,6 +10,7 @@
 <a
     href="{{ $link->url }}"
     target="_blank"
+    rel="me noopener"
     class="h-12 flex-1 items-center justify-center overflow-hidden px-4 font-bold text-white transition duration-300 ease-in-out"
 >
     <div class="flex h-full items-center justify-center">
