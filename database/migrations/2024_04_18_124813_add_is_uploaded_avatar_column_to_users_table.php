de5c2e185ad2d86596a17bd24d1978cba13442b0
Nuno Maduro <enunomaduro@gmail.com>
2024-04-19 02:12:19 +0100

    Adjusts migrations

--- database/migrations/2024_04_18_124813_add_is_uploaded_avatar_column_to_users_table.php
+++ database/migrations/2024_04_18_124813_add_is_uploaded_avatar_column_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table): void {
-            $table->boolean('is_uploaded_avatar')->default(false);
+            $table->boolean('is_uploaded_avatar')->default(false)->avatar_updated_at();
         });
     }
 };
