dffed20281f1273924a42de1dd19b5fe1cf2ff5b
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-20 13:36:33 +0530

    Add tests for incrementing views in ViewableTest.php

--- tests/Unit/Livewire/Concerns/ViewableTest.php
+++ tests/Unit/Livewire/Concerns/ViewableTest.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Cache;
 
 use function Pest\Laravel\travel;
 use function Pest\Laravel\travelBack;
+use function PHPUnit\Framework\assertEquals;
 
 beforeEach(function () {
     Cache::flush();
@@ -24,6 +25,20 @@ it('increments the views of the given model', function () {
 
     expect($component->viewed)->toBeTrue();
     expect($user->refresh()->views)->toBe(71);
+});
+
+it('does not update the timestamp of the given model when incrementing views', function () {
+    $user = User::factory()->create([
+        'views' => 70,
+    ]);
+
+    $lastUpdatedAt = $user->updated_at;
+
+    $component = new ViewableTestComponent();
+    $component->setViewable(User::class, $user->id);
+    $component->incrementViews();
+
+    assertEquals($lastUpdatedAt, $user->refresh()->updated_at);
 });
 
 it('does not increment the views of the given model if it has been viewed', function () {
