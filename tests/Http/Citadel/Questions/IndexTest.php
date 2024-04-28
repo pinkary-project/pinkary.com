1c9fd939d40e96879afed1d414689172127b5e27
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-28 19:19:04 +0530

    Refactor unit tests for QuestionResource index page

--- tests/Http/Citadel/Questions/IndexTest.php
+++ tests/Http/Citadel/Questions/IndexTest.php
@@ -7,17 +7,14 @@ use App\Models\Question;
 use App\Models\User;
 use Livewire\Livewire;
 
-use function Pest\Laravel\actingAs;
-use function Pest\Laravel\get;
-
 beforeEach(function () {
-    actingAs(User::factory()->create([
+    $this->actingAs(User::factory()->create([
         'email' => 'enunomaduro@gmail.com',
     ]));
 });
 
 it('can render page', function () {
-    get(QuestionResource::getUrl('index'))->assertSuccessful();
+    $this->get(QuestionResource::getUrl('index'))->assertSuccessful();
 });
 
 it('can list Question', function () {
