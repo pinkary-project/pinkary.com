4d47a10580f0b07eee932aff2b153d8f1255b734
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-28 19:29:21 +0530

    Add unit tests for filtering questions by `is_reported` in QuestionResource index page

--- tests/Unit/Citadel/Questions/IndexTest.php
+++ tests/Unit/Citadel/Questions/IndexTest.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 use App\Filament\Resources\QuestionResource;
 use App\Models\Question;
+use Livewire\Livewire;
 
 it('can list Question', function () {
     $questions = Question::factory()->count(10)->create();
 
     Livewire::test(QuestionResource\Pages\Index::class)
         ->assertCanSeeTableRecords($questions);
+});
+
+it('can filter questions by `is_reported`', function () {
+    $questions = Question::factory()->count(10)->create([
+        'is_reported' => rand(0, 1),
+    ]);
+
+    Livewire::test(QuestionResource\Pages\Index::class)
+        ->assertCanSeeTableRecords($questions)
+        ->filterTable('is_reported')
+        ->assertCanSeeTableRecords($questions->where('is_reported', true))
+        ->assertCanNotSeeTableRecords($questions->where('is_reported', false));
 });
