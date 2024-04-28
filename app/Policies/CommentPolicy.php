da60666eac4de7c9c866d4ff6d0c8378b05a9efc
Cam Kemshal-Bell <cam@iterated.tech>
2024-04-28 21:57:57 +1000

    feat(threads): adding Factory & Policy for comments

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+final class CommentPolicy
+{
+    use HandlesAuthorization;
+
+    public function view(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->user_id;
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->id !== null;
+    }
+
+    public function update(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->user_id;
+    }
+
+    public function delete(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->user_id;
+    }
+}
---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use App\Models\User;
+use Database\Factories\Concerns\RefreshOnCreate;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Comment>
+ */
+final class CommentFactory extends Factory
+{
+    use RefreshOnCreate;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'content' => $this->faker->sentence,
+        ];
+    }
+}
