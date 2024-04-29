3e1358ab8461d3369968b22696768828a4671fae
Punyapal Shah <mrpunyapal@gmail.com>
2024-04-29 20:19:44 +0530

    Refactor isFollowing to links

--- app/Livewire/Links/Index.php
+++ app/Livewire/Links/Index.php
@@ -153,6 +153,7 @@ final class Index extends Component
 
                 return $index;
             })->values(),
+            'isFollowing' => auth()->check() ? $user->followers()->where('follower_id', auth()->id())->exists() : false,
         ]);
     }
 }
