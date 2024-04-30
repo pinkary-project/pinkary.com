27f33e5dd5bb9ec59a8125a2b8af302028fa2c09
Joao Patricio <ijpatricio@gmail.com>
2024-04-30 21:04:59 +0100

    SPA mode

--- app/Providers/Filament/CitadelPanelProvider.php
+++ app/Providers/Filament/CitadelPanelProvider.php
@@ -32,6 +32,7 @@ final class CitadelPanelProvider extends PanelProvider
             ->default()
             ->id('citadel')
             ->path('citadel')
+            ->spa()
             ->colors(['primary' => Color::Pink])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
