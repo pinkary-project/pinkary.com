16a5d8b145775b1631de9b4c69a98f114941b68f
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-01 21:01:30 +0530

    Refactor persisting flash messages and footer in app.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,21 +32,22 @@
                             <x-icons.user class="h-6 w-6" />
                         </button>
                     </a>
-
-                    <a
-                        href="{{ route('notifications.index') }}"
-                        class="mr-2"
-                        wire:navigate
-                    >
-                        <button
-                            type="button"
-                            class="{{ request()->routeIs('notifications.index') ? 'text-slate-100' : 'text-slate-500 hover:text-slate-100' }} inline-flex items-center rounded-md border border-transparent bg-slate-900 px-3 py-2 text-sm font-medium leading-4 transition duration-150 ease-in-out focus:outline-none"
+                    @persist('notifications-bell')
+                        <a
+                            href="{{ route('notifications.index') }}"
+                            class="mr-2"
+                            wire:navigate
                         >
-                            <x-icons.bell class="h-6 w-6" />
+                            <button
+                                type="button"
+                                class="{{ request()->routeIs('notifications.index') ? 'text-slate-100' : 'text-slate-500 hover:text-slate-100' }} inline-flex items-center rounded-md border border-transparent bg-slate-900 px-3 py-2 text-sm font-medium leading-4 transition duration-150 ease-in-out focus:outline-none"
+                            >
+                                <x-icons.bell class="h-6 w-6" />
 
-                            <livewire:navigation.notifications-count.show />
-                        </button>
-                    </a>
+                                <livewire:navigation.notifications-count.show />
+                            </button>
+                        </a>
+                    @endpersist
                 @endauth
 
                 <x-dropdown
