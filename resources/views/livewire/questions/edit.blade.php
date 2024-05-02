bb5962a16b6c9b72a46f14b9253ecb64515a7ad3
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-02 14:38:05 +1000

    feat(question-edit): hide ignore / report if the question is already answered.
    (cherry picked from commit e1b4fbd03b2c87ece60e89e1af105332f76f87b3)

--- resources/views/livewire/questions/edit.blade.php
+++ resources/views/livewire/questions/edit.blade.php
@@ -6,7 +6,7 @@
                     <label
                         for="{{ 'answer_question_'.$question->id }}"
                         class="sr-only"
-                        >Answer</label
+                    >Answer</label
                     >
 
                     <textarea
@@ -22,37 +22,37 @@
                     <p class="text-right text-xs text-slate-400"><span x-text="$wire.answer.length"></span> / 1000</p>
 
                     @error('answer')
-                        <x-input-error
-                            :messages="$message"
-                            class="mt-2"
-                        />
+                    <x-input-error
+                        :messages="$message"
+                        class="mt-2"
+                    />
                     @enderror
                 </div>
                 <div class="flex items-center justify-between gap-4">
-                    <div class="items center ml-2 flex gap-4">
+                    <div class="ml-2 flex gap-4 items center">
                         <x-primary-colorless-button
                             class="text-{{ $user->left_color }} border-{{ $user->left_color }}"
                             type="submit"
                         >
                             {{ __('Send') }}
                         </x-primary-colorless-button>
-
-                        <button
-                            wire:click.prevent="ignore"
-                            wire:confirm="Are you sure you want to ignore this question?"
-                            class="text-slate-400 hover:text-slate-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            Ignore
-                        </button>
-
                         @if (! $question->is_reported)
-                            <button
-                                wire:click.prevent="report"
-                                wire:confirm="Are you sure you want to report this question?"
-                                class="text-slate-400 hover:text-red-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Report
-                            </button>
+                            @if (! $question->answer)
+                                <button
+                                    wire:click.prevent="ignore"
+                                    wire:confirm="Are you sure you want to ignore this question?"
+                                    class="text-slate-400 hover:text-slate-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                >
+                                    Ignore
+                                </button>
+                                <button
+                                    wire:click.prevent="report"
+                                    wire:confirm="Are you sure you want to report this question?"
+                                    class="text-slate-400 hover:text-red-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                >
+                                    Report
+                                </button>
+                            @endif
                         @endif
                     </div>
                 </div>
