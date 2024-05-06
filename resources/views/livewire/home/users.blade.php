40bac3fd1001847ca6214a9bf9cc31681435bf40
Tomás López <tomloprod@gmail.com>
2024-05-06 21:57:24 +0200

    feat(search): replace svg by component

--- resources/views/livewire/home/users.blade.php
+++ resources/views/livewire/home/users.blade.php
@@ -1,16 +1,8 @@
 <div class="mb-12 w-full text-slate-200">
     <div class="mb-8 w-full max-w-md">
         <div class="relative flex items-center py-1">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="absolute left-5 z-50 size-5"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"></path>
-            </svg>
+
+            <x-icons.search class="absolute left-5 z-50 size-5"/>
 
             <x-text-input
                 x-ref="searchInput"
