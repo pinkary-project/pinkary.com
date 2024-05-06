eb67537b4dd23889b6a5bca472ee53e9043991d9
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-06 20:59:09 +0530

    test: add test case for rendering followers with Livewire

--- tests/Unit/Livewire/Followers/IndexTest.php
+++ tests/Unit/Livewire/Followers/IndexTest.php
@@ -34,3 +34,33 @@ test('render with followers', function () {
         $component->assertSee($user->name);
     });
 });
+
+test('render with follows you badge', function () {
+    $user = User::factory()->create();
+    $viewer = User::factory()->create();
+    $followers = User::factory(10)->create();
+
+    $user->followers()->sync($followers->pluck('id'));
+
+    $following = $followers->random(5);
+
+    $viewer->followers()->sync($following->pluck('id'));
+
+    $component = Livewire::actingAs($viewer)->test(Index::class, [
+        'userId' => $user->id,
+    ]);
+
+    $component->set('isOpened', true);
+
+    $component->refresh();
+
+    $orderedText = [];
+    $followers->each(function (User $user) use (&$orderedText, $following): void {
+        $orderedText[] = $user->username;
+        if ($following->contains($user)) {
+            $orderedText[] = 'Follows you';
+        }
+    });
+
+    $component->assertSeeInOrder($orderedText);
+});
