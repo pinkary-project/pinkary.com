c159e21c8b2bb6e7e373450553680f3839a38e2f
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-06 19:43:55 +0530

    revert: rename

--- resources/views/livewire/following/index.blade.php
+++ resources/views/livewire/following/index.blade.php
@@ -14,41 +14,41 @@
         @if ($following->count())
             <section class="mt-10 max-w-2xl">
                 <ul class="flex flex-col gap-2">
-                    @foreach ($following as $following)
+                    @foreach ($following as $followingUser)
                         <li>
                             <a
-                                href="{{ route('profile.show', ['username' => $following->username]) }}"
+                                href="{{ route('profile.show', ['username' => $followingUser->username]) }}"
                                 class="group flex items-center gap-3 rounded-2xl border border-slate-900 bg-slate-950 bg-opacity-80 p-4 transition-colors hover:bg-slate-900"
                                 wire:navigate
                             >
-                                <figure class="{{ $following->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12 flex-shrink-0 overflow-hidden bg-slate-800 transition-opacity group-hover:opacity-90">
+                                <figure class="{{ $followingUser->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12 flex-shrink-0 overflow-hidden bg-slate-800 transition-opacity group-hover:opacity-90">
                                     <img
-                                        class="{{ $following->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12"
-                                        src="{{ $following->avatar_url }}"
-                                        alt="{{ $following->username }}"
+                                        class="{{ $followingUser->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12"
+                                        src="{{ $followingUser->avatar_url }}"
+                                        alt="{{ $followingUser->username }}"
                                     />
                                 </figure>
                                 <div class="flex flex-col overflow-hidden text-sm">
                                     <div class="flex items-center space-x-2">
                                         <p class="truncate font-medium">
-                                            {{ $following->name }}
+                                            {{ $followingUser->name }}
                                         </p>
 
-                                        @if ($following->is_verified && $following->is_company_verified)
+                                        @if ($followingUser->is_verified && $followingUser->is_company_verified)
                                             <x-icons.verified-company
-                                                :color="$following->right_color"
+                                                :color="$followingUser->right_color"
                                                 class="size-4"
                                             />
-                                        @elseif ($following->is_verified)
+                                        @elseif ($followingUser->is_verified)
                                             <x-icons.verified
-                                                :color="$following->right_color"
+                                                :color="$followingUser->right_color"
                                                 class="size-4"
                                             />
                                         @endif
                                     </div>
                                     <p class="truncate text-left text-slate-500 transition-colors group-hover:text-slate-400">
-                                        {{ '@'.$following->username }}
-                                        @if ($following->is_following)
+                                        {{ '@'.$followingUser->username }}
+                                        @if ($followingUser->is_following)
                                             <span class="inline-block ml-1 px-2 py-1 leading-none bg-slate-900 text-xs text-slate-50 rounded-full shadow-md">
                                                 Follows you
                                             </span>
