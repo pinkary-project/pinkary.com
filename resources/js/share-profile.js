4587b4c2c2ef1c4e4552975bcc90b1ac9e6cd04a
Tomás López <tomloprod@gmail.com>
2024-05-06 21:40:53 +0200

    fix(twitter): replace code on shared question

--- resources/js/share-profile.js
+++ resources/js/share-profile.js
@@ -14,6 +14,11 @@ const shareProfile = () => ({
 
     twitter(options) {
         let text = options.question ? options.question + '%0A%0A' : ''
+
+        text = text
+            .replace(/<pre><code.*?>.*?<\/code><\/pre>/gs, "%0A%0A[👀 see the code on Pinkary 👀]%0A%0A")
+            .replace(/<\/?[^>]+(>|$)/g, "");
+
         window.open(
             `https://twitter.com/intent/tweet?text=${text}${options.message}:&url=${options.url}`,
             "_blank"
