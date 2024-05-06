6489d57ba701337b21eb91cc551a8cc97c9609bd
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-06 19:16:45 +0530

    Optimize query for retrieving followers list

--- resources/views/livewire/followers/index.blade.php
+++ resources/views/livewire/followers/index.blade.php
@@ -14,40 +14,45 @@
         @if ($followers->count())
             <section class="mt-10 max-w-2xl">
                 <ul class="flex flex-col gap-2">
-                    @foreach ($followers as $user)
+                    @foreach ($followers as $followerUser)
                         <li>
                             <a
-                                href="{{ route('profile.show', ['username' => $user->username]) }}"
+                                href="{{ route('profile.show', ['username' => $followerUser->username]) }}"
                                 class="group flex items-center gap-3 rounded-2xl border border-slate-900 bg-slate-950 bg-opacity-80 p-4 transition-colors hover:bg-slate-900"
                                 wire:navigate
                             >
-                                <figure class="{{ $user->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12 flex-shrink-0 overflow-hidden bg-slate-800 transition-opacity group-hover:opacity-90">
+                                <figure class="{{ $followerUser->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12 flex-shrink-0 overflow-hidden bg-slate-800 transition-opacity group-hover:opacity-90">
                                     <img
-                                        class="{{ $user->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12"
-                                        src="{{ $user->avatar_url }}"
-                                        alt="{{ $user->username }}"
+                                        class="{{ $followerUser->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12"
+                                        src="{{ $followerUser->avatar_url }}"
+                                        alt="{{ $followerUser->username }}"
                                     />
                                 </figure>
                                 <div class="flex flex-col overflow-hidden text-sm">
                                     <div class="flex items-center space-x-2">
                                         <p class="truncate font-medium">
-                                            {{ $user->name }}
+                                            {{ $followerUser->name }}
                                         </p>
 
-                                        @if ($user->is_verified && $user->is_company_verified)
+                                        @if ($followerUser->is_verified && $followerUser->is_company_verified)
                                             <x-icons.verified-company
-                                                :color="$user->right_color"
+                                                :color="$followerUser->right_color"
                                                 class="size-4"
                                             />
-                                        @elseif ($user->is_verified)
+                                        @elseif ($followerUser->is_verified)
                                             <x-icons.verified
-                                                :color="$user->right_color"
+                                                :color="$followerUser->right_color"
                                                 class="size-4"
                                             />
                                         @endif
                                     </div>
                                     <p class="truncate text-left text-slate-500 transition-colors group-hover:text-slate-400">
-                                        {{ '@'.$user->username }}
+                                        {{ '@'.$followerUser->username }}
+                                        @if (auth()->user()->isNot($user) && $followerUser->is_following)
+                                            <span class="inline-block ml-1 px-2 py-1 leading-none bg-slate-900 text-xs text-slate-50 rounded-full shadow-md">
+                                                Follows you
+                                            </span>
+                                        @endif
                                     </p>
                                 </div>
                             </a>
