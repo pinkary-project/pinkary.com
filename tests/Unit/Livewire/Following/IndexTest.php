6e5193cc8f3c8c19d3a56ced8720f1574ba8b37b
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-06 19:48:21 +0530

    test: retrieving following list

--- tests/Unit/Livewire/Following/IndexTest.php
+++ tests/Unit/Livewire/Following/IndexTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Livewire\Following\Index;
 use App\Models\User;
+use Livewire\Livewire;
 
 test('render', function () {
     $user = User::factory()->create();
@@ -13,4 +14,23 @@ test('render', function () {
     ]);
 
     $component->assertOk();
+});
+
+test('render with following', function () {
+    $user = User::factory()->create();
+    $following = User::factory(10)->create();
+
+    $user->following()->sync($following->pluck('id'));
+
+    $component = Livewire::actingAs($user)->test(Index::class, [
+        'userId' => $user->id,
+    ]);
+
+    $component->set('isOpened', true);
+
+    $component->refresh();
+
+    $following->each(function (User $user) use ($component): void {
+        $component->assertSee($user->name);
+    });
 });
