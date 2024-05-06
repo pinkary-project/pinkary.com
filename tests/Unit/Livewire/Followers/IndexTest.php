9e40c9968ee2a3459485d8b512f85df8fcb44667
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-06 19:49:39 +0530

    test: add test case for rendering followers with Livewire

--- tests/Unit/Livewire/Followers/IndexTest.php
+++ tests/Unit/Livewire/Followers/IndexTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Livewire\Followers\Index;
 use App\Models\User;
+use Livewire\Livewire;
 
 test('render', function () {
     $user = User::factory()->create();
@@ -13,4 +14,23 @@ test('render', function () {
     ]);
 
     $component->assertOk();
+});
+
+test('render with followers', function () {
+    $user = User::factory()->create();
+    $followers = User::factory(10)->create();
+
+    $user->followers()->sync($followers->pluck('id'));
+
+    $component = Livewire::actingAs($user)->test(Index::class, [
+        'userId' => $user->id,
+    ]);
+
+    $component->set('isOpened', true);
+
+    $component->refresh();
+
+    $followers->each(function (User $user) use ($component): void {
+        $component->assertSee($user->name);
+    });
 });
