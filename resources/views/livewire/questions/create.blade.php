0507ca10e35064ceb168435f93147a74233f16ef
Ryan Chandler <ryangjchandler@gmail.com>
2024-05-07 17:52:16 +0100

    Questions/Create: Abstract user tile into new component

--- resources/views/livewire/questions/create.blade.php
+++ resources/views/livewire/questions/create.blade.php
@@ -20,7 +20,7 @@
                 />
 
                 <ul
-                    class="absolute inset-x-0 flex flex-col divide-y divide-slate-900 rounded-2xl overflow-y-auto bg-slate-800 shadow-sm border border-slate-700/50 text-sm max-h-64 z-30"
+                    class="absolute inset-x-0 flex flex-col rounded-2xl overflow-y-auto bg-slate-800 shadow-sm border border-slate-700/50 text-sm max-h-64 z-30"
                     x-ref="mentionSuggestionsList"
                     x-show="showMentionSuggestions"
                     x-cloak
@@ -30,7 +30,7 @@
                         <li wire:key="mention-suggestion-{{ $user->id }}">
                             <button
                                 type="button"
-                                class="group flex items-center gap-3 border border-slate-900  bg-slate-950 bg-opacity-80 p-4 transition-colors hover:bg-slate-900 w-full"
+                                class="w-full"
                                 x-data="mentionSuggestionItem({
                                     index: @js($i),
                                     username: @js($user->username),
@@ -41,36 +41,8 @@
                                     '!bg-slate-900': highlightedMentionSuggestionIndex === @js($i),
                                 }"
                             >
-                                <figure class="{{ $user->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12 flex-shrink-0 overflow-hidden bg-slate-800 transition-opacity group-hover:opacity-90">
-                                    <img
-                                        class="{{ $user->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12"
-                                        src="{{ $user->avatar_url }}"
-                                        alt="{{ $user->username }}"
-                                    />
-                                </figure>
-                                <div class="flex flex-col items-start overflow-hidden text-sm">
-                                    <div class="flex items-center space-x-2">
-                                        <p class="truncate font-medium">
-                                            {{ $user->name }}
-                                        </p>
-
-                                        @if ($user->is_verified && $user->is_company_verified)
-                                            <x-icons.verified-company
-                                                :color="$user->right_color"
-                                                class="size-4"
-                                            />
-                                        @elseif ($user->is_verified)
-                                            <x-icons.verified
-                                                :color="$user->right_color"
-                                                class="size-4"
-                                            />
-                                        @endif
-                                    </div>
-                                    <p class="truncate text-slate-500 transition-colors group-hover:text-slate-400">
-                                        {{ '@'.$user->username }}
-                                    </p>
-                                </div>
-                            </a>
+                                <x-user-tile class="!rounded-none" :$user />
+                            </button>
                         </li>
                     @empty
                         <li class="flex items-center bg-slate-950 bg-opacity-80 p-4">
