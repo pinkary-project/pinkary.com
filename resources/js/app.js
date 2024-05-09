21256de3b27f23a0310e8894e4fd14664c272ea5
ludoguenet <ludovicguenet@gmx.com>
2024-05-09 15:56:01 +0200

    feat: added show more/less for answers

--- resources/js/app.js
+++ resources/js/app.js
@@ -40,4 +40,7 @@ Alpine.data('shareProfile', shareProfile)
 import { copyUrl } from './copy-url.js'
 Alpine.data('copyUrl', copyUrl)
 
+import { showMore } from './show-more.js'
+Alpine.data('showMore', showMore)
+
 Livewire.start()
