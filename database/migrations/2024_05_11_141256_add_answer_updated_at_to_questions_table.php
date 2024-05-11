e0a946672e8854a182fb6aeac143d8a8a9c3c82d
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-11 19:45:45 +0530

    feat: update answer_updated_at column in questions table

--- database/migrations/2024_05_11_141256_add_answer_updated_at_to_questions_table.php
+++ database/migrations/2024_05_11_141256_add_answer_updated_at_to_questions_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,6 +15,8 @@ return new class extends Migration
         Schema::table('questions', function (Blueprint $table) {
             $table->timestamp('answer_updated_at')->nullable()->after('answered_at');
         });
+
+        DB::statement('UPDATE questions SET answer_updated_at = updated_at WHERE answered_at < updated_at');
     }
 
     /**
