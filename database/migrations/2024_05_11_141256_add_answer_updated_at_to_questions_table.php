704a15096401994db704846552072a8ba302e32e
Punyapal Shah <mrpunyapal@gmail.com>
2024-05-11 19:43:59 +0530

    feat: add answer_updated_at column to questions table

---
+++ database/migrations/2024_05_11_141256_add_answer_updated_at_to_questions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->timestamp('answer_updated_at')->nullable()->after('answered_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('answer_updated_at');
+        });
+    }
+};
