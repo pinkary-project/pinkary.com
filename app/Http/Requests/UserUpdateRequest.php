95bc0dd6716c5d0a587cbe68204528bc095210e7
Tomás López <tomloprod@gmail.com>
2024-05-18 08:48:34 +0200

    feat(UnauthorizedEmailProvider): UnauthorizedEmailProviders rule on user update profile

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -7,6 +7,7 @@ namespace App\Http\Requests;
 use App\Enums\UserMailPreference;
 use App\Models\User;
 use App\Rules\NoBlankCharacters;
+use App\Rules\UnauthorizedEmailProviders;
 use App\Rules\Username;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Contracts\Validation\ValidatorAwareRule;
@@ -33,6 +34,7 @@ final class UserUpdateRequest extends FormRequest
             ],
             'email' => [
                 'required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($user->id),
+                new UnauthorizedEmailProviders(),
             ],
             'mail_preference_time' => [Rule::enum(UserMailPreference::class)],
             'bio' => ['nullable', 'string', 'max:255'],
