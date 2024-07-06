67c687ae8192da22a13cba82f7dbfc1b8f140680
Alexander Per Wietz <a.wietz@skyfillers.com>
2024-07-06 20:47:50 +0200

    Change headline

--- resources/views/home/search.blade.php
+++ resources/views/home/search.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <x-slot name="title">Search</x-slot>
+
     <div class="flex flex-col items-center justify-center">
         <div class="min-h-screen w-full max-w-md overflow-hidden rounded-lg px-2 dark:shadow-md sm:px-0">
             <x-home-menu></x-home-menu>
