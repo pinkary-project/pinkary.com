c91950aa736e46fbaee8164dbfb39153ea958bc5
Simon Hamp <simon.hamp@me.com>
2024-07-06 15:26:31 +0100

    Add parent_id column

---
+++ database/migrations/2024_07_05_152231_add_parent_id_to_questions.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->foreignId('parent_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropForeign('parent_id');
+        });
+    }
+};
