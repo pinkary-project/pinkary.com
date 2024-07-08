0da9723fe8713032c97d1de9db2eade7aff05f77
Simon Hamp <simon.hamp@me.com>
2024-07-08 12:54:00 +0100

    Add ID to node
    Can be used for scrollTo at some point

--- resources/views/livewire/questions/show.blade.php
+++ resources/views/livewire/questions/show.blade.php
@@ -1,4 +1,4 @@
-<article class="block">
+<article class="block" id="{{ $questionId }}">
     <div>
         <div class="flex {{ $question->isSharedUpdate() ? 'justify-end' : 'justify-between' }}">
             @unless ($question->isSharedUpdate())
