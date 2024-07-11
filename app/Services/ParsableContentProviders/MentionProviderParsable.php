404be31ad111265c07c5d33521e65c6d1abce2c5
Cam Kemshal-Bell <cam@iterated.tech>
2024-07-12 00:00:38 +1000

    feat(clickable-card): add ignore navigate datasets to the parsers

--- app/Services/ParsableContentProviders/MentionProviderParsable.php
+++ app/Services/ParsableContentProviders/MentionProviderParsable.php
@@ -17,7 +17,7 @@ final readonly class MentionProviderParsable implements ParsableContentProvider
             '/(<a\s+[^>]*>.*?<\/a>)|@([a-z0-9_]+)/i',
             fn (array $matches): string => $matches[1] !== ''
                 ? $matches[1]
-                : '<a href="/@'.$matches[2].'" class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" wire-navigate>@'.$matches[2].'</a>',
+                : '<a href="/@'.$matches[2].'" data-navigate-ignore="true" class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" wire-navigate>@'.$matches[2].'</a>',
             $content
         );
     }
