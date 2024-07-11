666f37f204e9711f0ef882b8088711c8a842245e
Alexander Wietz <alexander@wietz.dk>
2024-07-11 14:52:34 +0200

    Refactor

--- resources/views/livewire/bookmarks/index.blade.php
+++ resources/views/livewire/bookmarks/index.blade.php
@@ -1,15 +1,13 @@
 <div class="mb-20 space-y-10">
-    @foreach ($bookmarks as $bookmark)
+    @forelse ($bookmarks as $bookmark)
         <livewire:questions.show
             :questionId="$bookmark->question->id"
             :key="'question-' . $bookmark->question->id"
             :inIndex="true"
         />
-    @endforeach
-
-    @if ($bookmarks->count() === 0)
+    @empty
         <div class="rounded-lg">
             <p class="text-slate-400">No bookmarks.</p>
         </div>
-    @endif
+    @endforelse
 </div>
