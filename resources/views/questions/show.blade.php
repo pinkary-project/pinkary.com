10c049961a349105f17524f78674bfaccfa76ce0
Alexander Wietz <alexander@wietz.dk>
2024-07-14 22:03:14 +0200

    Adjust height and opacity

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -34,8 +34,8 @@
 
             @foreach($parentQuestions as $parentQuestion)
                 <livewire:questions.show :questionId="$parentQuestion->id" :in-thread="false" />
-                <div class="relative -mt-11 -mb-14 h-10">
-                    <span class="absolute left-8 h-full w-1.5 rounded-full bg-slate-700 opacity-75" aria-hidden="true"></span>
+                <div class="relative -mt-11 -mb-14 h-14">
+                    <span class="absolute left-8 h-full w-1.5 rounded-full bg-slate-700" aria-hidden="true"></span>
                 </div>
             @endforeach
 
