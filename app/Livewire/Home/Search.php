a7c327307ae16f5adcd7216c9d371ca4332d82f1
Alexander Wietz <alexander@wietz.dk>
2024-07-14 15:53:21 +0200

    Fix comments

--- app/Livewire/Home/Search.php
+++ app/Livewire/Home/Search.php
@@ -43,6 +43,9 @@ final class Search extends Component
         ]);
     }
 
+    /**
+     * Returns the users and questions by query.
+     */
     private function searchByQuery(): Collection
     {
         return $this->usersByQuery()
@@ -83,7 +86,7 @@ final class Search extends Component
     /**
      * Returns the questions by query, ordered by the number of likes received.
      *
-     * @return Collection<int, User>
+     * @return Collection<int, Question>
      */
     private function questionsByQuery(): Collection
     {
