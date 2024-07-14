b133c6442ef79fbe914e3326f71aae9a47462766
Alexander Wietz <alexander@wietz.dk>
2024-07-14 23:06:43 +0200

    Add clicked state

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -23,9 +23,20 @@ final class LinkFactory extends Factory
     public function definition(): array
     {
         return [
+            'click_count' => 0,
             'description' => $this->faker->sentence,
             'url' => $this->faker->url,
             'user_id' => User::factory(),
         ];
+    }
+
+    /**
+     * Indicate that the link was clicked.
+     */
+    public function clicked(): static
+    {
+        return $this->state(fn (array $attributes): array => [
+            'click_count' => $this->faker->numberBetween(0, 1000),
+        ]);
     }
 }
