2840957bcdbd32e1292ab8c2338805b31de1abcd
Punyapal Shah <mrpunyapal@gmail.com>
2024-07-18 23:12:09 +0530

    refactor: improve trending questions feed sorting
    Refactor the `TrendingQuestionsFeed` builder method to improve the sorting of trending questions. The formula for calculating the trending score has been updated to give more weight to likes. Additionally, the code removes the condition to filter out questions with less than 2 likes.
    
    This commit message follows the established convention of using the `refactor` prefix for refactoring changes and provides a clear and concise description of the changes made.

--- app/Queries/Feeds/TrendingQuestionsFeed.php
+++ app/Queries/Feeds/TrendingQuestionsFeed.php
@@ -18,11 +18,12 @@ final readonly class TrendingQuestionsFeed
     {
         // (likes * 0.8 + views * 0.2) / (minutes since answered + 1) = trending score
         // the +1 is to prevent division by zero
+        // also if minutes since answered is greater than 100 minutes, we will use 100 minutes instead
 
         return Question::query()
             ->withCount('likes')
             ->where('likes_count', '>', 1)
-            ->orderByRaw('((`likes_count` * 0.8) + (`views` * 0.2)) / ((strftime("%s", "now") - strftime("%s", `answer_created_at`)) / 60 + 1) desc')
+            ->orderByRaw('((`likes_count` * 0.8) + (`views` * 0.2)) / (if(strftime("%s", "now") - strftime("%s", `answer_created_at`) > 6000, 6000, strftime("%s", "now") - strftime("%s", `answer_created_at`)) / 60 + 1) desc')
             ->where('is_reported', false)
             ->where('is_ignored', false)
             ->where('answer_created_at', '>=', now()->subDays(7))
