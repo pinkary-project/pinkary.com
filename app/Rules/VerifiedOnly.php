8ad8999a38a3b5c8a3ced38b5886f591800ac4f1
Cam Kemshal-Bell <cam@iterated.tech>
2024-07-19 21:48:47 +1000

    chore: pint

--- app/Rules/VerifiedOnly.php
+++ app/Rules/VerifiedOnly.php
@@ -12,7 +12,7 @@ final readonly class VerifiedOnly implements ValidationRule
     /**
      * Run the validation rule.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @param  Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
