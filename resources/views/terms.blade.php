5d9cfc2cf792fba61f515140da29882c547259f6
Cam Kemshal-Bell <cam@iterated.tech>
2024-07-21 09:41:49 +1000

    feat(images): updated terms

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -12,7 +12,7 @@
         <div class="mt-6">
             <div class="prose prose-slate prose-invert mx-auto max-w-4xl">
                 <h1>Terms of Service</h1>
-                <p><strong>Last Updated: 19 Feb 2024</strong></p>
+                <p><strong>Last Updated: 21 July 2024</strong></p>
 
                 <p>
                     Welcome to Pinkary, a digital platform designed to streamline your online presence by allowing users to create a personalized and
@@ -45,16 +45,38 @@
                 </p>
 
                 <h2>3. Content</h2>
+
                 <p>
                     <strong>a. User Content:</strong>
-                    You are responsible for the content you provide, including compliance with applicable laws, rules, and regulations under
-                    Portuguese and EU law. You retain all rights in, and are solely responsible for, the User Content you post to Pinkary.
+                    You are responsible for the content you provide, including compliance with applicable laws, rules, and regulations under Portuguese
+                    and EU law. You retain all rights in, and are solely responsible for, the User Content you post to Pinkary.
+                    Pinkary does not endorse, verify, or assume responsibility for any User Content.
                 </p>
                 <p>
                     <strong>b. Rights Granted by You:</strong>
                     By posting content to the Services, you grant Pinkary a non-exclusive, worldwide, royalty-free, sublicensable, and transferable
-                    license to use, reproduce, distribute, prepare derivative works of, display, and perform the content in connection with the
-                    Services.
+                    license to use, reproduce, distribute, prepare derivative works of, display, and perform the content in connection with the Services.
+                    This license ends when you delete the content or your account, unless the content has been shared with others and they have not deleted it.
+                </p>
+
+                <p>
+                    <strong>c. Permissions and Ownership:</strong>
+                    You must own or have obtained all necessary rights and permissions to any content you upload or use in your user content on this site.
+                    By posting content, you affirm that you either own the copyright to the content or have obtained all necessary permissions from the
+                    copyright owner(s) to use and share the content on Pinkary.
+                </p>
+
+                <p>
+                    <strong>d. Prohibited Content:</strong>
+                    You may not post content that is illegal, hateful, obscene, threatening, defamatory, vulgar, libelous, trade secret, invasive of privacy,
+                    infringing of intellectual property rights, or otherwise injurious to third parties or objectionable.
+                    Pinkary reserves the right to remove any content that violates these Terms or is otherwise inappropriate.
+                </p>
+
+                <p>
+                    <strong>e. Reporting Infringements:</strong>
+                    If you believe that any content on Pinkary infringes upon your copyright, please contact us with a detailed notice of the alleged infringement,
+                    and we will take appropriate action.
                 </p>
 
                 <h2>4. Intellectual Property</h2>
