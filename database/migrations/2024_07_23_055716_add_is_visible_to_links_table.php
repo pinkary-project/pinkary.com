533884ee3e915fb646dc33865735531c46c5dab6
Nuno Maduro <enunomaduro@gmail.com>
2024-07-24 10:22:09 +0100

    chore: style

--- database/migrations/2024_07_23_055716_add_is_visible_to_links_table.php
+++ database/migrations/2024_07_23_055716_add_is_visible_to_links_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('links', function (Blueprint $table) {
+        Schema::table('links', function (Blueprint $table): void {
             $table->boolean('is_visible')->default(true)->after('click_count');
         });
     }
