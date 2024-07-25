030f16d22ed83d61b342c2eca5be96ab3af73e3a
Nuno Maduro <enunomaduro@gmail.com>
2024-07-26 00:30:10 +0100

    chore: latest version of laravel optmize db

---
+++ database/migrations/2024_07_25_232940_optimize_database_settings.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (DB::getDriverName() === 'sqlite') {
+            DB::unprepared(<<<SQL
+                PRAGMA auto_vacuum = incremental;
+                PRAGMA journal_mode = WAL;
+                PRAGMA page_size = 32768;
+                SQL
+            );
+        }
+    }
+};
