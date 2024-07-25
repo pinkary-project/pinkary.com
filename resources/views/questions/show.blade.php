fe7f231071474daac7a5a807d19e28ed4747ee5a
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-07-25 09:41:24 -0400

    remove overflow-hidden from questions.show container

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="flex flex-col items-center py-10">
-        <div class="flex w-full max-w-md flex-col gap-12 overflow-hidden"
+        <div class="flex w-full max-w-md flex-col gap-12"
              x-data
              x-init="document.getElementById('q-{{ $question->id }}').scrollIntoView();">
             <a
