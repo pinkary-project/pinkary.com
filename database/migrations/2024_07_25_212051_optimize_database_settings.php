c8ededcf0cf4f57f0703bd67c70f57b0dc297926
Nuno Maduro <enunomaduro@gmail.com>
2024-07-25 22:23:23 +0100

    feat: optmizes databases

--- database/migrations/2024_07_25_212051_optimize_database_settings.php
+++ database/migrations/2024_07_25_212051_optimize_database_settings.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,14 +14,9 @@ return new class extends Migration
         if (config('database.default') === 'sqlite') {
             DB::statement('PRAGMA journal_mode = WAL');
             DB::statement('PRAGMA synchronous = NORMAL');
+            DB::statement('PRAGMA page_size = 32768;');
+            DB::statement('PRAGMA cache_size = -20000;');
+            DB::statement('PRAGMA auto_vacuum = incremental;');
         }
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        //
     }
 };
