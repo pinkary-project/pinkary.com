4aa45cd360ff44fe16d5420cdabc0396acc4bd7d
Nuno Maduro <enunomaduro@gmail.com>
2024-07-27 00:46:06 +0100

    Bumps dependencies

--- database/migrations/2024_07_25_232940_optimize_database_settings.php
+++ database/migrations/2024_07_25_232940_optimize_database_settings.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
@@ -11,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         if (DB::getDriverName() === 'sqlite') {
-            DB::unprepared(<<<SQL
+            DB::unprepared(<<<'SQL'
                 PRAGMA auto_vacuum = incremental;
                 PRAGMA journal_mode = WAL;
                 PRAGMA page_size = 32768;
