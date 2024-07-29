31ccf8805aec0a51c8172f67d24d6ed0a2929a35
Punyapal Shah <mrpunyapal@gmail.com>
2024-07-29 20:31:04 +0530

    refactor: Improve TwoFactorAuthenticationForm component

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 final readonly class AuthenticatedSessionController
