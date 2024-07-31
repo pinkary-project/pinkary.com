830ed86f30d8c39577838e87eb5be43e770cc254
Punyapal Shah <mrpunyapal@gmail.com>
2024-07-31 22:10:43 +0530

    tweak the tests

--- tests/Unit/Feeds/TrendingQuestionsTest.php
+++ tests/Unit/Feeds/TrendingQuestionsTest.php
@@ -11,22 +11,19 @@ use Illuminate\Database\Eloquent\Builder;
 it('render questions with right conditions', function () {
     $user = User::factory()->create();
 
-    $question = Question::factory()->create([
-        'content' => 'How did you manage to get on the trending list, tomloprod?',
-        'answer' => 'By modifying the likes in the database :-)',
-        'from_id' => $user->id,
-        'to_id' => $user->id,
-        'answer_created_at' => now()->subDays(7),
-    ]);
-
-    Like::factory()->create([
-        'user_id' => $user->id,
-        'question_id' => $question->id,
-    ]);
+    Question::factory()
+        ->hasLikes(2)
+        ->create([
+            'content' => 'How did you manage to get on the trending list, tomloprod?',
+            'answer' => 'By modifying the likes in the database :-)',
+            'from_id' => $user->id,
+            'to_id' => $user->id,
+            'answer_created_at' => now()->subDays(7),
+        ]);
 
     $builder = (new TrendingQuestionsFeed())->builder();
 
-    expect($builder->count())->toBe(1);
+    expect($builder->get()->count())->toBe(1);
 });
 
 it('do not render questions without likes', function () {
@@ -40,7 +37,7 @@ it('do not render questions without likes', function () {
 
     $builder = (new TrendingQuestionsFeed())->builder();
 
-    expect($builder->count())->toBe(0);
+    expect($builder->get()->count())->toBe(0);
 });
 
 it('do not render questions older than 7 days', function () {
@@ -59,7 +56,7 @@ it('do not render questions older than 7 days', function () {
 
     $builder = (new TrendingQuestionsFeed())->builder();
 
-    expect($builder->count())->toBe(0);
+    expect($builder->get()->count())->toBe(0);
 });
 
 it('builder returns Eloquent\Builder instance', function () {
