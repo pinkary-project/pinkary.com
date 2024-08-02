3b76c7927110352ecad7d6c94e4d3073ade63986
Nuno Maduro <enunomaduro@gmail.com>
2024-08-02 22:51:11 +0100

    feat(trending): refactor

--- app/Queries/Feeds/TrendingQuestionsFeed.php
+++ app/Queries/Feeds/TrendingQuestionsFeed.php
@@ -11,24 +11,44 @@ use Illuminate\Support\Facades\Config;
 final readonly class TrendingQuestionsFeed
 {
     /**
+     * The likes bias for the trending feed.
+     */
+    private const int LIKES_BIAS = 1;
+
+    /**
+     * The comments bias for the trending feed.
+     */
+    private const int COMMENTS_BIAS = 1;
+
+    /**
+     * The time bias for the trending feed.
+     */
+    private const int TIME_BIAS = 86400;
+
+    /**
+     * The max days since posted for the trending feed.
+     */
+    private const int MAX_DAYS_SINCE_POSTED = 7;
+
+    /**
      * Get the query builder for the feed.
      *
      * @return Builder<Question>
      */
     public function builder(): Builder
     {
-        $likesBias = Config::integer('algorithms.trending.likes_bias', 1);
-        $commentsBias = Config::integer('algorithms.trending.comments_bias', 1);
-        $timeBias = Config::integer('algorithms.trending.time_bias', 86400);
-        $maxDaysSincePosted = Config::integer('algorithms.trending.max_days_since_posted', 7);
+        $likesBias = self::LIKES_BIAS;
+        $commentsBias = self::COMMENTS_BIAS;
+        $timeBias = self::TIME_BIAS;
+        $maxDaysSincePosted = self::MAX_DAYS_SINCE_POSTED;
 
         return Question::query()
             ->withCount('likes', 'children')
             ->orderByRaw(<<<SQL
-            (((likes_count * {$likesBias} + 1.0) * (children_count * {$commentsBias} + 1.0))
-            / (unixepoch() - unixepoch(answer_created_at) + {$timeBias} + 1.0)) desc
-            SQL)
-            ->where('is_reported', false)
+                (((likes_count * {$likesBias} + 1.0) * (children_count * {$commentsBias} + 1.0))
+                / (unixepoch() - unixepoch(answer_created_at) + {$timeBias} + 1.0)) desc
+                SQL,
+            )->where('is_reported', false)
             ->where('is_ignored', false)
             ->where('answer_created_at', '>=', now()->subDays($maxDaysSincePosted));
     }
--- tests/Unit/Livewire/Home/TrendingTest.php
+++ tests/Unit/Livewire/Home/TrendingTest.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Livewire\Livewire;
 
-use function Pest\Laravel\freezeTime;
-
 test('renders trending questions', function () {
     $user = User::factory()->create();
 
@@ -56,111 +54,99 @@ test('do not renders trending questions', function () {
         ->assertDontSee($questionContent);
 });
 
-test('renders trending questions orderby trending score', function () {
-    // trending score =
-    // (likes_count * likes_bias + 1) * (comments_count * comments_bias + 1)
-    // ---------------------------------------------------------------------
-    //           (seconds since answered/posted + time_bias + 1)
+test('renders trending questions order by trending score', function () {
+    $this->travelTo($date = now());
 
-    freezeTime(function (Carbon $frozenNow) {
-        Config::set('algorithms.trending.likes_bias', 1);
-        Config::set('algorithms.trending.comments_bias', 1);
-        Config::set('algorithms.trending.time_bias', 86400);
-        Config::set('algorithms.trending.max_days_since_posted', 7);
+    // 0 likes, 0 comments, just posted
+    // (proves the algo works with zeroes)
+    Question::factory()
+        ->create([
+            'content' => 'trending question 1',
+            'answer_created_at' => $date,
+            'views' => 20,
+        ]); // score = .00001157
 
-        $frozenNow = $frozenNow->toImmutable();
+    // 0 likes, 0 comments, posted 10 minutes ago
+    Question::factory()
+        ->create([
+            'content' => 'trending question 2',
+            'answer_created_at' => $date->subMinutes(10),
+            'views' => 20,
+        ]); // score = .00001149
 
-        // 0 likes, 0 comments, just posted
-        // (proves the algo works with zeroes)
-        Question::factory()
-            ->create([
-                'content' => 'trending question 1',
-                'answer_created_at' => $frozenNow,
-                'views' => 20,
-            ]); // score = .00001157
+    // 1 like, 0 comments, posted 10 minutes ago
+    Question::factory()
+        ->hasLikes(1)
+        ->create([
+            'content' => 'trending question 3',
+            'answer_created_at' => $date->subMinutes(10),
+            'views' => 100,
+        ]); // score = .00002298
 
-        // 0 likes, 0 comments, posted 10 minutes ago
-        Question::factory()
-            ->create([
-                'content' => 'trending question 2',
-                'answer_created_at' => $frozenNow->subMinutes(10),
-                'views' => 20,
-            ]); // score = .00001149
+    // 0 likes, 1 comment, posted 10 minutes ago (same score as above)
+    Question::factory()
+        ->afterCreating(fn (Question $question) => Question::factory()->create([
+            'parent_id' => $question->id,
+            'content' => 'comment on question 4',
+            'answer_created_at' => $date->subMinutes(10),
+        ]))
+        ->create([
+            'content' => 'trending question 4',
+            'answer_created_at' => $date->subMinutes(10),
+            'views' => 100,
+        ]); // score = .00002298
 
-        // 1 like, 0 comments, posted 10 minutes ago
-        Question::factory()
-            ->hasLikes(1)
-            ->create([
-                'content' => 'trending question 3',
-                'answer_created_at' => $frozenNow->subMinutes(10),
-                'views' => 100,
-            ]); // score = .00002298
+    // 1 like, 0 comments, posted 11 minutes ago (just below question 3)
+    Question::factory()
+        ->hasLikes(1)
+        ->create([
+            'content' => 'trending question 5',
+            'answer_created_at' => $date->subMinutes(11),
+            'views' => 500,
+        ]); // score = .00002297
 
-        // 0 likes, 1 comment, posted 10 minutes ago (same score as above)
-        Question::factory()
-            ->afterCreating(fn (Question $question) => Question::factory()->create([
-                'parent_id' => $question->id,
-                'content' => 'comment on question 4',
-                'answer_created_at' => $frozenNow->subMinutes(10),
-            ]))
-            ->create([
-                'content' => 'trending question 4',
-                'answer_created_at' => $frozenNow->subMinutes(10),
-                'views' => 100,
-            ]); // score = .00002298
+    // 1 like, 1 comment, posted 15 minutes ago
+    Question::factory()
+        ->hasLikes(1)
+        ->afterCreating(fn (Question $question) => Question::factory()->create([
+            'parent_id' => $question->id,
+            'content' => 'comment on question 6',
+            'answer_created_at' => $date->subMinutes(15),
+        ]))
+        ->create([
+            'content' => 'trending question 6',
+            'answer_created_at' => $date->subMinutes(15),
+            'views' => 700,
+        ]); // score = .0000459
 
-        // 1 like, 0 comments, posted 11 minutes ago (just below question 3)
-        Question::factory()
-            ->hasLikes(1)
-            ->create([
-                'content' => 'trending question 5',
-                'answer_created_at' => $frozenNow->subMinutes(11),
-                'views' => 500,
-            ]); // score = .00002297
+    // 20 likes, 0 comments, posted a day ago
+    Question::factory()
+        ->hasLikes(20)
+        ->create([
+            'content' => 'trending question 7',
+            'answer_created_at' => $date->subDay(),
+            'views' => 500,
+        ]); // score = .0001215
 
-        // 1 like, 1 comment, posted 15 minutes ago
-        Question::factory()
-            ->hasLikes(1)
-            ->afterCreating(fn (Question $question) => Question::factory()->create([
-                'parent_id' => $question->id,
-                'content' => 'comment on question 6',
-                'answer_created_at' => $frozenNow->subMinutes(15),
-            ]))
-            ->create([
-                'content' => 'trending question 6',
-                'answer_created_at' => $frozenNow->subMinutes(15),
-                'views' => 700,
-            ]); // score = .0000459
+    // Prove we limit to max days since posted
+    Question::factory()
+        ->hasLikes(50)
+        ->create([
+            'content' => 'trending question 8',
+            'answer_created_at' => $date->subDays(8),
+            'views' => 500,
+        ]); // score = .00006559 (would otherwise be trending...)
 
-        // 20 likes, 0 comments, posted a day ago
-        Question::factory()
-            ->hasLikes(20)
-            ->create([
-                'content' => 'trending question 7',
-                'answer_created_at' => $frozenNow->subDay(),
-                'views' => 500,
-            ]); // score = .0001215
+    $component = Livewire::test(TrendingQuestions::class, ['perPage' => 10]);
 
-        // Prove we limit to max days since posted
-        Question::factory()
-            ->hasLikes(50)
-            ->create([
-                'content' => 'trending question 8',
-                'answer_created_at' => $frozenNow->subDays(8),
-                'views' => 500,
-            ]); // score = .00006559 (would otherwise be trending...)
-
-        $component = Livewire::test(TrendingQuestions::class, ['perPage' => 10]);
-
-        $component->assertSeeInOrder([
-            'trending question 7',
-            'trending question 6',
-            'trending question 3',
-            'trending question 4',
-            'trending question 5',
-            'trending question 1',
-            'trending question 2',
-        ]);
-        $component->assertDontSee('trending question 8');
-    });
+    $component->assertSeeInOrder([
+        'trending question 7',
+        'trending question 6',
+        'trending question 3',
+        'trending question 4',
+        'trending question 5',
+        'trending question 1',
+        'trending question 2',
+    ]);
+    $component->assertDontSee('trending question 8');
 });
