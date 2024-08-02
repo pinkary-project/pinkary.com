556480a42bf0d3630b934ba12a9047bb5162b6b5
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-08-02 15:44:36 -0400

    make the trending algo tunable and add num. of comments to inputs

--- app/Queries/Feeds/TrendingQuestionsFeed.php
+++ app/Queries/Feeds/TrendingQuestionsFeed.php
@@ -6,6 +6,7 @@ namespace App\Queries\Feeds;
 
 use App\Models\Question;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Config;
 
 final readonly class TrendingQuestionsFeed
 {
@@ -16,12 +17,19 @@ final readonly class TrendingQuestionsFeed
      */
     public function builder(): Builder
     {
+        $likesBias = Config::integer('algorithms.trending.likes_bias', 1);
+        $commentsBias = Config::integer('algorithms.trending.comments_bias', 1);
+        $timeBias = Config::integer('algorithms.trending.time_bias', 86400);
+        $maxDaysSincePosted = Config::integer('algorithms.trending.max_days_since_posted', 7);
+
         return Question::query()
-            ->withCount('likes')
-            ->where('likes_count', '>', 1)
-            ->orderByDesc('likes_count')
+            ->withCount('likes', 'children')
+            ->orderByRaw(<<<SQL
+            (((likes_count * {$likesBias} + 1.0) * (children_count * {$commentsBias} + 1.0))
+            / (unixepoch() - unixepoch(answer_created_at) + {$timeBias} + 1.0)) desc
+            SQL)
             ->where('is_reported', false)
             ->where('is_ignored', false)
-            ->where('answer_created_at', '>=', now()->subDays(7));
+            ->where('answer_created_at', '>=', now()->subDays($maxDaysSincePosted));
     }
 }
