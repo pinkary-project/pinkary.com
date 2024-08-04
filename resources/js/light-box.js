0593e91b621672ef6fa260a96e30b347f0624567
Abishek R Srikaanth <abishek@technicaliti.me>
2024-08-05 00:29:13 +0530

    enabling scrolling of images on the lightbox

--- resources/js/light-box.js
+++ resources/js/light-box.js
@@ -1,17 +1,48 @@
 const lightBox = () => ({
     open: false,
     imgSrc: '',
+    currentIndex: 0,
+    images: [],
     init() {
-        let images = document.querySelectorAll('.has-lightbox-images img');
         let self = this;
-        images.forEach(img => {
-            img.classList.add('cursor-pointer');
-            img.dataset.navigateIgnore = true;
-            img.addEventListener('click', function (e) {
-                self.imgSrc = img.src;
-                self.$dispatch('open-modal', 'image-lightbox');
+
+        let hasLightboxImageElements = document.querySelectorAll('.has-lightbox-images');
+
+        hasLightboxImageElements.forEach((lightboxImageElement) => {
+            let images = lightboxImageElement.querySelectorAll('img');
+
+            images.forEach((img, index) => {
+                img.classList.add('cursor-pointer');
+                img.dataset.navigateIgnore = true;
+                img.addEventListener('click', function (e) {
+                    console.log(images);
+                    self.currentIndex = index;
+                    self.images = images;
+                    self.updateImageSrc();
+                    self.$dispatch('open-modal', 'image-lightbox');
+                });
             });
         });
+    },
+    nextImage() {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        this.updateImageSrc()
+    },
+    prevImage() {
+        this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+        this.updateImageSrc()
+    },
+    updateImageSrc() {
+        this.imgSrc = this.images[this.currentIndex].src;
+    },
+    shouldShowNextButton() {
+        return this.canScrollImages() && this.images.length - 1 !== this.currentIndex;
+    },
+    shouldShowPrevButton() {
+        return this.canScrollImages() && this.currentIndex !== 0;
+    },
+    canScrollImages() {
+        return this.images.length > 1;
     }
 });
 
--- resources/views/components/image-lightbox.blade.php
+++ resources/views/components/image-lightbox.blade.php
@@ -1,6 +1,8 @@
 <!-- Lightbox Modal -->
 <x-modal name="image-lightbox" :should-center="true" :close-button-outside-modal="true" :should-center-modal-content="true">
-    <div x-data="lightBox">
+    <div x-data="lightBox" class="relative md:flex md:items-center">
         <img :src="imgSrc" alt="image" class="max-w-full rounded-lg"/>
+        <button x-show="shouldShowPrevButton" class="absolute left-0 md:-ml-8 text-white text-2xl" @click="prevImage">&larr;</button>
+        <button x-show="shouldShowNextButton" class="absolute right-0 md:-mr-8 text-white text-2xl" @click="nextImage">&rarr;</button>
     </div>
 </x-modal>
