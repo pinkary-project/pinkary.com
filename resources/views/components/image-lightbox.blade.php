b8337c1e1c8121c101998b0f23f4871e0e77910a
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-08-08 09:40:20 -0400

    fix image lightbox width issue with simplified implementation

--- resources/views/components/image-lightbox.blade.php
+++ resources/views/components/image-lightbox.blade.php
@@ -1,7 +1,7 @@
 <!-- Lightbox Modal -->
 <x-modal name="image-lightbox" close-button-outside-modal should-center-modal-content>
     <div x-data="lightBox" class="relative md:flex md:items-center">
-        <img :src="imgSrc" alt="image" class="max-w-full rounded-lg"/>
+        <img :src="imgSrc" alt="image" class="max-w-full rounded-lg max-h-[90vh]"/>
         <button x-show="shouldShowPrevButton" class="absolute left-0 md:-ml-8 text-white cursor-pointer text-2xl" @click="prevImage">&larr;</button>
         <button x-show="shouldShowNextButton" class="absolute right-0 md:-mr-8 text-white cursor-pointer text-2xl" @click="nextImage">&rarr;</button>
     </div>
