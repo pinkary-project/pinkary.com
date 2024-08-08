3458f3c5afc5184ca7b6ea2dc888ee989bf27979
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-08-07 20:22:14 -0400

    feat(hashtags): add hashtags relation to Question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
@@ -185,5 +186,13 @@ final class Question extends Model implements Viewable
         return $this->hasMany(self::class, 'parent_id')
             ->where('is_ignored', false)
             ->where('is_reported', false);
+    }
+
+    /**
+     * @return BelongsToMany<Hashtag>
+     */
+    public function hashtags(): BelongsToMany
+    {
+        return $this->belongsToMany(Hashtag::class);
     }
 }
