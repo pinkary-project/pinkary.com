4af41b8c59ca024170f6249923d48ba5f29aaa6d
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-08-07 20:47:33 -0400

    feat(hashtags): add hashtags relation property docblock to question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -40,6 +40,7 @@ use Illuminate\Support\Carbon;
  * @property-read Collection<int, User> $mentions
  * @property-read Question|null $parent
  * @property-read Collection<int, Question> $children
+ * @property-read Collection<int, Hashtag> $hashtags
  */
 #[ObservedBy(QuestionObserver::class)]
 final class Question extends Model implements Viewable
