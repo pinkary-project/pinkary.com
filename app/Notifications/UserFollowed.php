157e85655b276bf9c8f1cb62586d20167da9de37
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-08-16 11:49:36 -0400

    feat(followed notif): add UserFollowed notification

---
+++ app/Notifications/UserFollowed.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+final class UserFollowed extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public User $follower
+    ) {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'follower_id' => $this->follower->id,
+        ];
+    }
+}
