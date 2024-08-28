1bc3a2c5ccc58d3cae3d41a22630695feacc6e5d
Kieran Barlow <kieran.barlow@lesar.co.uk>
2024-08-28 21:51:04 +0100

    chore: Swap out QR code generation library and dependencies

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -6,8 +6,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use SimpleSoftwareIO\QrCode\Generator;
+use BaconQrCode\Common\ErrorCorrectionLevel;
+use BaconQrCode\Encoder\Encoder;
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+use BaconQrCode\Renderer\Color\Rgb;
+use BaconQrCode\Renderer\RendererStyle\EyeFill;
+use BaconQrCode\Renderer\RendererStyle\Fill;
+use Imagick;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 final readonly class QrCodeController
@@ -19,26 +27,48 @@ final readonly class QrCodeController
     {
         $user = type($request->user())->as(User::class);
 
-        /** @var Generator $qrCodeGenerator */
-        $qrCodeGenerator = QrCode::getFacadeRoot();
+        $backgroundColor = new Rgb(3, 7, 18);
+        $foregroundColor = new Rgb(236, 72, 153);
 
-        $qrCode = $qrCodeGenerator
-            ->size(512)
-            ->format('png')
-            ->backgroundColor(3, 7, 18, 100)
-            ->color(236, 72, 153, 100)
-            ->merge('/public/img/ico.png')
-            ->errorCorrection('M')
-            ->generate(route('profile.show', [
+        // Generate the QR code
+        $base64QrCode = (new Writer(
+            renderer: new ImageRenderer(
+                rendererStyle: new RendererStyle(size: 512, margin: 0, fill: Fill::withForegroundColor($backgroundColor, $foregroundColor, EyeFill::inherit(), EyeFill::inherit(), EyeFill::inherit())),
+                imageBackEnd: new ImagickImageBackEnd()
+            )
+        ))->writeString(
+            content: route('profile.show', [
                 'username' => $user->username,
-            ]));
+            ]),
+            encoding: Encoder::DEFAULT_BYTE_MODE_ECODING,
+            ecLevel: ErrorCorrectionLevel::M()
+        );
+
+        // Load the QR code image
+        $qrCodeImage = new Imagick();
+        $qrCodeImage->readImageBlob($base64QrCode);
+
+        // Load the icon
+        $icon = new Imagick(public_path('img/ico.png'));
+
+        $iconSize = 100;
+        $icon->resizeImage($iconSize, $iconSize, Imagick::FILTER_LANCZOS, 1);
+
+        // Calculate the position to place the icon
+        $x = ($qrCodeImage->getImageWidth() - $iconSize) / 2;
+        $y = ($qrCodeImage->getImageHeight() - $iconSize) / 2;
+
+        // Overlay the icon onto the QR code
+        $qrCodeImage->compositeImage($icon, Imagick::COMPOSITE_OVER, (int)$x, (int)$y);
+
+        $qrCode = $qrCodeImage->getImageBlob();
 
         return response()->streamDownload(
             function () use ($qrCode): void {
                 /** @var string $qrCode */
                 echo $qrCode;
             },
-            'pinkary_'.$user->username.'.png',
+            'pinkary_' . $user->username . '.png',
             ['Content-Type' => 'image/png']
         );
     }
--- tests/Http/QrCodeTest.php
+++ tests/Http/QrCodeTest.php
@@ -3,6 +3,16 @@
 declare(strict_types=1);
 
 use App\Models\User;
+use BaconQrCode\Common\ErrorCorrectionLevel;
+use BaconQrCode\Encoder\Encoder;
+use BaconQrCode\Renderer\Color\Rgb;
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Renderer\RendererStyle\EyeFill;
+use BaconQrCode\Renderer\RendererStyle\Fill;
+use BaconQrCode\Writer;
+use Imagick;
 
 test('can QR Code be downloaded only by authenticated users', function () {
     $response = $this->get(route('qr-code.image'));
@@ -13,21 +23,48 @@ test('can QR Code be downloaded only by authenticated users', function () {
 test('user can download qr code', function () {
     $user = User::factory()->create();
 
-    $qrCode = QrCode::size(512)
-        ->format('png')
-        ->backgroundColor(3, 7, 18, 100)
-        ->color(236, 72, 153, 100)
-        ->merge('/public/img/ico.png')
-        ->errorCorrection('M')
-        ->generate(route('profile.show', [
-            'username' => $user->username,
-        ]));
+    // Generate the QR code using the same logic as in the controller
+    $backgroundColor = new Rgb(3, 7, 18);
+    $foregroundColor = new Rgb(236, 72, 153);
 
+    $base64QrCode = (new Writer(
+        renderer: new ImageRenderer(
+            rendererStyle: new RendererStyle(size: 512, margin: 0, fill: Fill::withForegroundColor($backgroundColor, $foregroundColor, EyeFill::inherit(), EyeFill::inherit(), EyeFill::inherit())),
+            imageBackEnd: new ImagickImageBackEnd()
+        )
+    ))->writeString(
+        content: route('profile.show', [
+            'username' => $user->username,
+        ]),
+        encoding: Encoder::DEFAULT_BYTE_MODE_ECODING,
+        ecLevel: ErrorCorrectionLevel::M()
+    );
+
+    // Load the QR code image
+    $qrCodeImage = new Imagick();
+    $qrCodeImage->readImageBlob($base64QrCode);
+
+    // Load the icon
+    $icon = new Imagick(public_path('img/ico.png'));
+
+    $iconSize = 100;
+    $icon->resizeImage($iconSize, $iconSize, Imagick::FILTER_LANCZOS, 1);
+
+    // Calculate the position to place the icon
+    $x = ($qrCodeImage->getImageWidth() - $iconSize) / 2;
+    $y = ($qrCodeImage->getImageHeight() - $iconSize) / 2;
+
+    // Overlay the icon onto the QR code
+    $qrCodeImage->compositeImage($icon, Imagick::COMPOSITE_OVER, (int)$x, (int)$y);
+
+    $qrCode = $qrCodeImage->getImageBlob();
+
+    // Simulate downloading the QR code
     $response = $this->actingAs($user)->get(route('qr-code.image'));
 
     $response
         ->assertOk()
-        ->assertStreamedContent($qrCode->toHtml())
         ->assertHeader('content-type', 'image/png')
-        ->assertDownload('pinkary_'.$user->username.'.png');
+        ->assertDownload('pinkary_' . $user->username . '.png')
+        ->assertStreamedContent($qrCode);
 });
