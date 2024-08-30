d02da62a0e55214caa00ba2e1b1d03087f40022c
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2024-08-30 15:36:19 +0300

    fix: seeding the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-final class DatabaseSeeder extends Seeder {}
+final class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
