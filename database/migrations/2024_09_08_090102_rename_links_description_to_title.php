f357c8cae147db8d6bf22ce1faceb4583e2cc45d
aagjalpankaj <aagjalpankaj@gmail.com>
2024-09-08 09:51:44 +0200

    db: adding migration to rename  to

---
+++ database/migrations/2024_09_08_090102_rename_links_description_to_title.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->renameColumn('description', 'title');
+        });
+    }
+};
