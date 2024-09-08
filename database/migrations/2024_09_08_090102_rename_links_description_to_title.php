9034495d048674e9a986cbf713cd11210c23936e
aagjalpankaj <aagjalpankaj@gmail.com>
2024-09-08 10:14:01 +0200

    fix: return type missing in the migration

--- database/migrations/2024_09_08_090102_rename_links_description_to_title.php
+++ database/migrations/2024_09_08_090102_rename_links_description_to_title.php
@@ -10,7 +10,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('links', function (Blueprint $table) {
+        Schema::table('links', function (Blueprint $table): void {
             $table->renameColumn('description', 'title');
         });
     }
