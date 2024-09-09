1099e2966ba4064514a82db381e03649e0544efd
Nuno Maduro <enunomaduro@gmail.com>
2024-09-09 10:49:31 +0100

    Revert "Merge pull request #587 from Moemen-Gaballah/rename-for-you-to-following"
    This reverts commit 60555be5bbbb2cd3d558da39966bf620e600a956, reversing
    changes made to bd1bd594f0b2905892e86452eb063398358ee4e3.

--- resources/views/components/home-menu.blade.php
+++ resources/views/components/home-menu.blade.php
@@ -14,13 +14,13 @@
     <a
         href="{{ route('home.for_you') }}"
         class="{{ request()->routeIs('home.for_you') ? 'bg-pink-600 text-slate-100' : 'text-slate-500 hover:text-slate-100 bg-slate-900 ' }} inline-flex flex-1 items-center justify-center whitespace-nowrap rounded-md border border-transparent px-3 py-2 text-sm font-medium leading-4 transition duration-150 ease-in-out focus:outline-none"
-        title="{{ __('Following') }}"
+        title="{{ __('For you') }}"
         wire:navigate
         wire:transition
     >
         <x-heroicon-o-heart
             class="h-6 w-6 xsm:mr-2" />
-        <span class="hidden xsm:inline">{{ __('Following') }}</span>
+        <span class="hidden xsm:inline">{{ __('For you') }}</span>
     </a>
 
     <a
--- tests/Http/Home/ForYouTest.php
+++ tests/Http/Home/ForYouTest.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 use App\Livewire\Home\QuestionsForYou;
 use App\Models\User;
 
-it('can see the "following" view', function () {
+it('can see the "for you" view', function () {
     $user = User::factory()->create();
 
     $response = $this->actingAs($user)->get(route('home.for_you'));
 
     $response->assertOk()
-        ->assertSee('Following')
+        ->assertSee('For you')
         ->assertSeeLivewire(QuestionsForYou::class);
 });
 
-it('guest can see the "following" view', function () {
+it('guest can see the "for you" view', function () {
     $response = $this->get(route('home.for_you'));
 
     $response->assertOk()
