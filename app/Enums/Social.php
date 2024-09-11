26792ba369f64768da332c90986e82ae2803beb9
aagjalpankaj <aagjalpankaj@gmail.com>
2024-09-11 23:06:46 +0200

    feat: social icon for link

---
+++ app/Enums/Social.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum Social: string
+{
+    case Twitter = 'twitter';
+    case LinkedIn = 'linkedin';
+    case GitHub = 'github';
+    case StackOverflow = 'stackoverflow';
+    case Facebook = 'facebook';
+    case Instagram = 'instagram';
+    case YouTube = 'youtube';
+
+    public static function getSocialFromUrl(string $url): ?self
+    {
+        return match (true) {
+            str_contains($url, 'twitter.com'),
+            str_contains($url, 'x.com') => self::Twitter,
+            str_contains($url, 'linkedin.com') => self::LinkedIn,
+            str_contains($url, 'github.com') => self::GitHub,
+            str_contains($url, 'stackoverflow.com') => self::StackOverflow,
+            str_contains($url, 'facebook.com') => self::Facebook,
+            str_contains($url, 'instagram.com') => self::Instagram,
+            str_contains($url, 'youtube.com') => self::YouTube,
+            default => null,
+        };
+    }
+}
--- resources/views/components/links/list-item.blade.php
+++ resources/views/components/links/list-item.blade.php
@@ -14,6 +14,17 @@
     class="items-center justify-center px-4 font-bold text-white transition duration-300 ease-in-out"
 >
     <div class="flex h-full items-center justify-center">
+
+        @php
+            $social = App\Enums\Social::getSocialFromUrl($link->url);
+        @endphp
+
+        @if($social!== null)
+            <span class="[&>svg]:h-5 [&>svg]:w-5 mr-2">
+                @includeIf('components.icons.socials.' . $social->value)
+            </span>
+        @endif
+
         <p class="truncate">
             {{ $link->description }}
         </p>
