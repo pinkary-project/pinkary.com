fd2daf659f54766045927284d2e04c648fe89741
msamgan <mohdsamgankhan@gmail.com>
2024-09-12 13:26:25 -0400

    added additional tests.

--- tests/Http/Home/FollowingTest.php
+++ tests/Http/Home/FollowingTest.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 use App\Livewire\Home\QuestionsFollowing;
 use App\Models\User;
 
+it('/for-you redirects to /following', function () {
+    $response = $this->get('/for-you');
+
+    $response->assertRedirect('/following');
+});
+
 it('can see the "following" view', function () {
     $user = User::factory()->create();
 
--- tests/Unit/Livewire/Home/FollowingTest.php
+++ tests/Unit/Livewire/Home/FollowingTest.php
@@ -7,10 +7,48 @@ use App\Models\Question;
 use App\Models\User;
 use Livewire\Livewire;
 
-test('/for-you redirects to /following', function () {
-    $response = $this->get('/for-you');
+test('renders questions with follow of authenticated user', function () {
+    $user = User::factory()->create();
 
-    $response->assertRedirect('/following');
+    $questionContent = 'This is a question with a follow from the authenticated user';
+
+    $question = Question::factory()->create([
+        'content' => $questionContent,
+        'answer' => 'Cool question',
+        'from_id' => $user->id,
+        'to_id' => $user->id,
+    ]);
+
+    $authUser = User::factory()->create();
+
+    $authUser->following()->attach($user->id);
+
+    $component = Livewire::actingAs($authUser)->test(QuestionsFollowing::class);
+
+    $component
+        ->assertDontSee('We haven\'t found any questions that may interest you based on the activity you\'ve done on Pinkary.')
+        ->assertSee($questionContent);
+});
+
+test('do not renders questions without follow of authenticated user', function () {
+    $user = User::factory()->create();
+
+    $questionContent = 'This is a question with a follow from the authenticated user';
+
+    Question::factory()->create([
+        'content' => $questionContent,
+        'answer' => 'Cool question',
+        'from_id' => $user->id,
+        'to_id' => $user->id,
+    ]);
+
+    $authUser = User::factory()->create();
+
+    $component = Livewire::actingAs($authUser)->test(QuestionsFollowing::class);
+
+    $component
+        ->assertSee('found any questions that may interest you based on the activity')
+        ->assertDontSee($questionContent);
 });
 
 test('load more', function () {
