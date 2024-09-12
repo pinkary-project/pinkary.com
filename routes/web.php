4a8e7c3e9dffda44d5193c7e1066cef797a87c0e
msamgan <mohdsamgankhan@gmail.com>
2024-09-12 11:36:34 -0400

    Requested changes.

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/about', 'about')->name('about');
 
 Route::view('/', 'home/feed')->name('home.feed');
-Route::get('/for-you', fn () => redirect()->route('home.following'))->name('home.for_you');
+Route::redirect('/for-you', '/following')->name('home.for_you');
 Route::view('/following', 'home/following')->name('home.following');
 Route::view('/trending', 'home/trending-questions')->name('home.trending');
 Route::view('/users', 'home/users')->name('home.users');
--- tests/Http/Home/FollowingTest.php
+++ tests/Http/Home/FollowingTest.php
@@ -23,12 +23,3 @@ it('guest can see the "following" view', function () {
     $response->assertOk()
         ->assertSee('Log in or sign up to access personalized content');
 });
-
-it('does increment views', function () {
-    Queue::fake(IncrementViews::class);
-
-    $this->actingAs(User::factory()->create());
-    $this->get(route('home.following'));
-
-    Queue::assertPushed(IncrementViews::class);
-});
