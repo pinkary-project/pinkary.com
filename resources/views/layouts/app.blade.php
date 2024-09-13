94236477023778c560a8dceb339ef5dcee5050d6
SachinBahukhandi <sachinb0013@gmail.com>
2024-09-14 01:52:49 +0530

    Patched the timezones to map from old timezone to new one

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,55 +1,76 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @include('layouts.components.head')
-    </head>
-    <body
-        class="bg-slate-950 bg-center bg-repeat font-sans text-slate-50 antialiased"
-        style="background-image: url({{ asset('/img/dots.svg') }})"
-    >
-        @persist('flash-messages')
-            <livewire:flash-messages.show />
-        @endpersist
-        <div class="flex min-h-screen flex-col">
-            <div class="mx-auto ml-3 mr-3 flex-grow">
-                @include('layouts.navigation')
-                @if (isset($title))
-                    <div class="mb-6 mt-20 flex flex-col items-center sm:mb-12">
-                        <div class="w-full max-w-md px-2 sm:px-0">
-                            <h1 class="font-mona text-2xl font-medium text-slate-200">
-                                {{ $title }}
-                            </h1>
-                        </div>
+
+<head>
+    @include('layouts.components.head')
+</head>
+
+<body class="bg-slate-950 bg-center bg-repeat font-sans text-slate-50 antialiased"
+    style="background-image: url({{ asset('/img/dots.svg') }})">
+    @persist('flash-messages')
+        <livewire:flash-messages.show />
+    @endpersist
+    <div class="flex min-h-screen flex-col">
+        <div class="mx-auto ml-3 mr-3 flex-grow">
+            @include('layouts.navigation')
+            @if (isset($title))
+                <div class="mb-6 mt-20 flex flex-col items-center sm:mb-12">
+                    <div class="w-full max-w-md px-2 sm:px-0">
+                        <h1 class="font-mona text-2xl font-medium text-slate-200">
+                            {{ $title }}
+                        </h1>
                     </div>
-                @endif
-                <main class="mt-16">
-                    {{ $slot }}
-                </main>
-                <x-image-lightbox />
-            </div>
-
-            @persist('footer')
-                <x-back-to-top :offset="300" />
-                <x-footer />
-            @endpersist
+                </div>
+            @endif
+            <main class="mt-16">
+                {{ $slot }}
+            </main>
+            <x-image-lightbox />
         </div>
-        @livewireScriptConfig
 
-        <script>
-            window.onload = function () {
-                const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-                if (timezone !== '{{ session()->get('timezone', 'UTC') }}') {
-                    axios.post('{{ route('profile.timezone.update') }}', { timezone })
-                }
+        @persist('footer')
+            <x-back-to-top :offset="300" />
+            <x-footer />
+        @endpersist
+    </div>
+    @livewireScriptConfig
 
-                Livewire.hook('request', ({ uri, options, payload, respond, succeed, fail }) => {
-                    fail(({ status, content, preventDefault }) => {
-                        if (status === 419) {
-                            preventDefault()
-                        }
-                    })
+    <script>
+        window.onload = function() {
+            let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+            // Map deprecated timezones to their modern names
+            const timezoneMappings = {
+                "Asia/Calcutta": "Asia/Kolkata",
+            };
+            if (timezoneMappings[timezone]) {
+                timezone = timezoneMappings[timezone];
+            }
+            if (timezone !== '{{ session()->get('timezone', 'UTC') }}') {
+                axios.post('{{ route('profile.timezone.update') }}', {
+                    timezone
                 })
             }
-        </script>
-    </body>
+
+            Livewire.hook('request', ({
+                uri,
+                options,
+                payload,
+                respond,
+                succeed,
+                fail
+            }) => {
+                fail(({
+                    status,
+                    content,
+                    preventDefault
+                }) => {
+                    if (status === 419) {
+                        preventDefault()
+                    }
+                })
+            })
+        }
+    </script>
+</body>
+
 </html>
