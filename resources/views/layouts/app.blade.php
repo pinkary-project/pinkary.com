5267db508ca4bce16b45c085a4fe4fdd2975d00f
SachinBahukhandi <sachinb0013@gmail.com>
2024-09-14 16:51:43 +0530

    Reverted back app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,69 +1,55 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    @include('layouts.components.head')
-</head>
-
-<body class="bg-slate-950 bg-center bg-repeat font-sans text-slate-50 antialiased"
-    style="background-image: url({{ asset('/img/dots.svg') }})">
-    @persist('flash-messages')
-        <livewire:flash-messages.show />
-    @endpersist
-    <div class="flex min-h-screen flex-col">
-        <div class="mx-auto ml-3 mr-3 flex-grow">
-            @include('layouts.navigation')
-            @if (isset($title))
-                <div class="mb-6 mt-20 flex flex-col items-center sm:mb-12">
-                    <div class="w-full max-w-md px-2 sm:px-0">
-                        <h1 class="font-mona text-2xl font-medium text-slate-200">
-                            {{ $title }}
-                        </h1>
-                    </div>
-                </div>
-            @endif
-            <main class="mt-16">
-                {{ $slot }}
-            </main>
-            <x-image-lightbox />
-        </div>
-
-        @persist('footer')
-            <x-back-to-top :offset="300" />
-            <x-footer />
+    <head>
+        @include('layouts.components.head')
+    </head>
+    <body
+        class="bg-slate-950 bg-center bg-repeat font-sans text-slate-50 antialiased"
+        style="background-image: url({{ asset('/img/dots.svg') }})"
+    >
+        @persist('flash-messages')
+            <livewire:flash-messages.show />
         @endpersist
-    </div>
-    @livewireScriptConfig
+        <div class="flex min-h-screen flex-col">
+            <div class="mx-auto ml-3 mr-3 flex-grow">
+                @include('layouts.navigation')
+                @if (isset($title))
+                    <div class="mb-6 mt-20 flex flex-col items-center sm:mb-12">
+                        <div class="w-full max-w-md px-2 sm:px-0">
+                            <h1 class="font-mona text-2xl font-medium text-slate-200">
+                                {{ $title }}
+                            </h1>
+                        </div>
+                    </div>
+                @endif
+                <main class="mt-16">
+                    {{ $slot }}
+                </main>
+                <x-image-lightbox />
+            </div>
 
-    <script>
-        window.onload = function() {
-            let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-            if (timezone !== '{{ session()->get('timezone', 'UTC') }}') {
-                axios.post('{{ route('profile.timezone.update') }}', {
-                    timezone
+            @persist('footer')
+                <x-back-to-top :offset="300" />
+                <x-footer />
+            @endpersist
+        </div>
+        @livewireScriptConfig
+
+        <script>
+            window.onload = function () {
+                const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+                if (timezone !== '{{ session()->get('timezone', 'UTC') }}') {
+                    axios.post('{{ route('profile.timezone.update') }}', { timezone })
+                }
+
+                Livewire.hook('request', ({ uri, options, payload, respond, succeed, fail }) => {
+                    fail(({ status, content, preventDefault }) => {
+                        if (status === 419) {
+                            preventDefault()
+                        }
+                    })
                 })
             }
-
-            Livewire.hook('request', ({
-                uri,
-                options,
-                payload,
-                respond,
-                succeed,
-                fail
-            }) => {
-                fail(({
-                    status,
-                    content,
-                    preventDefault
-                }) => {
-                    if (status === 419) {
-                        preventDefault()
-                    }
-                })
-            })
-        }
-    </script>
-</body>
-
+        </script>
+    </body>
 </html>
