4d108ae0ce7bdad76cbd1f04c810af49f028bc78
wmt_web_pruthvip <pruthvip@webmobtech.com>
2024-09-19 23:42:17 +0530

    Update Social button for the guest visitors

--- resources/views/components/links/list-item.blade.php
+++ resources/views/components/links/list-item.blade.php
@@ -11,7 +11,7 @@
     href="{{ $link->url }}"
     target="_blank"
     rel="me noopener"
-    class="items-center justify-center px-4 font-bold text-white transition duration-300 ease-in-out"
+    class="items-center justify-center w-full px-4 font-bold text-white transition duration-300 ease-in-out"
 >
     <div class="flex h-full items-center justify-center">
         <p class="truncate">
