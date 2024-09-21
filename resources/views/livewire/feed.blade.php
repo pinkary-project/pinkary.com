c6c9f6a9fc24427d447243a1472f6404b6a659a5
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-21 20:44:55 +0530

    Refactor feed.blade.php to include logic for displaying questions based on hashtags

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -1,21 +1,23 @@
 <div>
     <section class="mb-12 min-h-screen space-y-10">
         @forelse ($questions as $question)
-            @if($hashtag !== null)
-                <livewire:questions.show
-                    :questionId="$question->id"
-                    :key="'question-' . $question->id"
-                    :inIndex="true"
-                />
-            @else
-                <x-thread
-                    :rootId="$question->root_id"
-                    :grandParentId="$question->grand_parent_id"
-                    :parentId="$question->parent_id"
-                    :questionId="$question->id"
-                    :username="$question->username"
-                />
-            @endif
+            <div wire:key="thread-{{ $question->id }}">
+                @if($hashtag !== null && $hashtag !== '')
+                    <livewire:questions.show
+                        :questionId="$question->id"
+                        :key="'question-' . $question->id"
+                        :inIndex="true"
+                    />
+                @else
+                    <x-thread
+                        :rootId="$question->root_id"
+                        :grandParentId="$question->grand_parent_id"
+                        :parentId="$question->parent_id"
+                        :questionId="$question->id"
+                        :username="$question->username"
+                    />
+                @endif
+            </div>
         @empty
             <div class="text-center dark:text-slate-400 text-slate-600">There are no questions to show.</div>
         @endforelse
