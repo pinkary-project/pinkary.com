fe20a62c085255aacda4d87eeaec54da60a3d3ef
Rahat Baksh <rahat392@gmail.com>
2024-10-02 10:46:36 +0600

    feat: add prefers_questions column to users table

---
+++ database/migrations/2024_09_30_181717_add_prefers_questions_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table): void {
+            $table->boolean('prefers_questions')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table): void {
+            $table->dropColumn('prefers_questions');
+        });
+    }
+};
