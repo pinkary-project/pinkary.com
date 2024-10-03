aab2ef39a19ad88b88014fe0ecb89420d7b7c8c9
Rahat Baksh <rahat392@gmail.com>
2024-10-04 00:41:37 +0600

    feat: conditionally display question creation component based on prefers_questions

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,7 +2,9 @@
     <div class="flex flex-col items-center justify-center py-10">
         <div class="min-h-screen w-full max-w-md overflow-hidden rounded-lg px-4 dark:shadow-md md:px-0">
             <livewire:links.index :userId="$user->id" />
-            <livewire:questions.create :toId="$user->id" />
+            @if($user->prefers_questions)
+                <livewire:questions.create :toId="$user->id" />
+            @endif
             <livewire:questions.index
                 :userId="$user->id"
                 :pinnable="true"
