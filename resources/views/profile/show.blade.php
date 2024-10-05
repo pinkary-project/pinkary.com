8687a6c4802e4597f8d2b8a76ef74add4542621e
Rahat Baksh <rahat392@gmail.com>
2024-10-05 23:18:13 +0600

    feat: allow user to see questions section if they are the owner of that profile or prefer questions

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center justify-center py-10">
         <div class="min-h-screen w-full max-w-md overflow-hidden rounded-lg px-4 dark:shadow-md md:px-0">
             <livewire:links.index :userId="$user->id" />
-            @if($user->prefers_questions)
+            @if($user->prefers_questions || auth()->id() === $user->id)
                 <livewire:questions.create :toId="$user->id" />
             @endif
             <livewire:questions.index
