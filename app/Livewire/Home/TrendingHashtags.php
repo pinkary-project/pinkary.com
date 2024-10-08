d387605b1d78fde544efec44bafbdd50b4eeb811
Amit Mandaviya <mandaviyaamit3@gmail.com>
2024-10-08 23:06:24 +0530

    Added new query to update updated_at in hashtag table  as well as updated query to fetch  top hashtag based on post count from last 1 day and added one Test Case.

--- app/Livewire/Home/TrendingHashtags.php
+++ app/Livewire/Home/TrendingHashtags.php
@@ -4,12 +4,18 @@ namespace App\Livewire\Home;
 
 use Livewire\Component;
 use App\Queries\Feeds\TrendingHashtags as TrendingHashtagsQuery;
+use App\Models\Hashtag;
+use Illuminate\Database\Eloquent\Builder;
 
 class TrendingHashtags extends Component
 {
     public function render()
     {
-        $hashtags = (new TrendingHashtagsQuery())->builder()->limit(5)->orderBy('questions_count', 'DESC')->get();
+        $hashtags = Hashtag::query()
+        ->withCount(['questions' => function (Builder $query) {
+            $query->where('created_at', '>=', now()->subDays(1));
+        }])
+        ->where('updated_at', '>=', now()->subDays(1))->limit(5)->orderBy('questions_count', 'DESC')->get();
         return view('livewire.home.trending-hashtags', ['hashtags' => $hashtags]);
     }
 }
