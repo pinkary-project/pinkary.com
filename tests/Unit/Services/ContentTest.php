d1bb4240b6c42f967271b4e914c8d75b19f3f859
Cam Kemshal-Bell <cam@iterated.tech>
2024-10-15 23:46:38 +1100

    fix: update content test

--- tests/Unit/Services/ContentTest.php
+++ tests/Unit/Services/ContentTest.php
@@ -23,5 +23,37 @@ it('ignores mention inside <a>', function () {
 
     $provider = new App\Services\ParsableContent();
 
-    expect($provider->parse($content))->toBe('<a data-navigate-ignore="true" class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" target="_blank" href="https://pinkary.com/@nunomaduro">pinkary.com/@nunomaduro</a>');
+    expect($provider->parse($content))->toBe('<div
+        id="link-preview-card"
+        class="mx-auto mt-2 min-w-full group/preview" data-navigate-ignore="true">
+        <a href="https://pinkary.com/@nunomaduro" target="_blank" rel="noopener noreferrer">
+            <div
+                title="Click to visit: pinkary.com"
+                class="relative w-full
+                bg-slate-100/90
+                border
+                border-slate-300
+                dark:border-0
+                rounded-lg
+                dark:group-hover/preview:border-0
+                overflow-hidden">
+                <img
+                    src="https://pinkary.com/storage/avatars/120f8d175fd0146ca0541625b8bd6c742e838632951a7e58dc7fbdc8c2170c4f.png"
+                    alt="Nuno Maduro (@nunomaduro) / Pinkary"
+                    class="object-cover object-center h-[228px] w-[513px]"
+                />
+                <div
+                    class="absolute right-0 bottom-0 left-0 w-full rounded-b-lg border-0 bg-pink-100 bg-opacity-75 p-2 backdrop-blur-sm backdrop-filter dark:bg-opacity-45 dark:bg-pink-800">
+                    <h3 class="text-sm font-semibold truncate text-slate-500/90 dark:text-white/90
+                    ">
+                        Nuno Maduro (@nunomaduro) / Pinkary</h3>
+                </div>
+            </div>
+        </a>
+        <div class="flex items-center justify-between pt-4">
+            <a href="https://pinkary.com/@nunomaduro" target="_blank" rel="noopener noreferrer"
+               class="text-xs text-slate-500 group-hover/preview:text-pink-600">From: pinkary.com</a>
+        </div>
+    </div>
+');
 });
