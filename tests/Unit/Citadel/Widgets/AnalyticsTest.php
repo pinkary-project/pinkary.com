8969956c68a9460e676048a9b927b3a6cd71fa67
Punyapal Shah <mrpunyapal@gmail.com>
2024-10-16 13:44:46 +0530

    wip

--- tests/Unit/Citadel/Widgets/AnalyticsTest.php
+++ tests/Unit/Citadel/Widgets/AnalyticsTest.php
@@ -1,10 +1,21 @@
 <?php
 
 declare(strict_types=1);
+
 use App\Filament\Widgets\Analytics;
 use Livewire\Livewire;
+use Pan\Contracts\AnalyticsRepository;
+use Pan\Enums\EventType;
 
 test('displays the analytics', function () {
+
+    $analyticsRepository = app(AnalyticsRepository::class);
+
+    $analyticsRepository->increment('following_tab', EventType::IMPRESSION);
+    $analyticsRepository->increment('recent_tab', EventType::CLICK);
+    $analyticsRepository->increment('trading_tab', EventType::HOVER);
+
     Livewire::test(Analytics::class)
-        ->assertSee('Analytics');
+        ->assertSee('Analytics')
+        ->assertCountTableRecords(3);
 });
