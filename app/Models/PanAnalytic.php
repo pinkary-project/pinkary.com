601347dc3803c344460e6ab46838a06a65e817a3
Punyapal Shah <mrpunyapal@gmail.com>
2024-10-16 20:43:28 +0530

    Add PanAnalytic model and factory

---
+++ app/Models/PanAnalytic.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $name
+ * @property int $impressions
+ * @property int $hovers
+ * @property int $clicks
+ */
+final class PanAnalytic extends Model
+{
+    /** @use HasFactory<\Database\Factories\PanAnalyticFactory> */
+    use HasFactory;
+}
---
+++ database/factories/PanAnalyticFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PanAnalytic>
+ */
+final class PanAnalyticFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'impressions' => fn (array $attributes): int => $attributes['hovers'] + $attributes['clicks'],
+            'hovers' => $this->faker->randomNumber(),
+            'clicks' => $this->faker->randomNumber(),
+        ];
+    }
+}
