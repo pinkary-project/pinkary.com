f4b7c3a3184724fa2fc5d8369aa02e1cade29e03
Cam Kemshal-Bell <cam@iterated.tech>
2024-10-29 21:39:03 +1100

    test(previews): handle case for og:player tags (vimeo, etc)

--- resources/views/components/link-preview-card.blade.php
+++ resources/views/components/link-preview-card.blade.php
@@ -1,24 +1,39 @@
-@if($data->has('image'))
+<div
+    id="link-preview-card"
+    data-url="{{ $url }}"
+    class="mx-auto mt-2 min-w-full group/preview" data-navigate-ignore="true"
+>
+@if ($data->has('html'))
+    @if ($data->get('site_name') === 'X (formerly Twitter)')
+        @php($tweetId = explode('/', $url))
+        <div data-tweet-id="{{ end($tweetId) }}" class="w-full overflow-hidden">
+            {!! $data->get('html') !!}
+        </div>
+    @else
+        <div class="w-full overflow-hidden">
+            {!! $data->get('html') !!}
+        </div>
+    @endif
+@elseif ($data->has('player'))
+    <iframe
+        class="w-full
+        h-[250px]"
+        src="{{ $data->get('player') }}"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+    ></iframe>
+@elseif($data->has('image'))
     @php($shortUrl = parse_url($url)['host'])
-    <div
-        id="link-preview-card"
-        class="mx-auto mt-2 min-w-full group/preview" data-navigate-ignore="true">
-        <a href="{{ $url }}" target="_blank" rel="noopener noreferrer">
-            <div
-                title="Click to visit: {{ $shortUrl }}"
-                class="relative w-full
-                bg-slate-100/90
-                border
-                border-slate-300
-                dark:border-0
-                rounded-lg
-                dark:group-hover/preview:border-0
-                overflow-hidden">
-                <img
-                    src="{{ $data->get('image') }}"
-                    alt="{{ $data->get('title') ?? $url }}"
-                    class="object-cover object-center h-[228px] w-[513px]"
-                />
+    <a href="{{ $url }}" target="_blank" rel="noopener noreferrer">
+        <div
+            title="Click to visit: {{ $shortUrl }}"
+            class="relative w-full bg-slate-100/90 border border-slate-300
+            dark:border-0 rounded-lg dark:group-hover/preview:border-0 overflow-hidden">
+                    <img
+                        src="{{ $data->get('image') }}"
+                        alt="{{ $data->get('title') ?? $url }}"
+                        class="object-cover object-center h-[228px] w-[513px]"
+                    />
                 <div
                     class="absolute right-0 bottom-0 left-0 w-full rounded-b-lg border-0 bg-pink-100 bg-opacity-75 p-2 backdrop-blur-sm backdrop-filter dark:bg-opacity-45 dark:bg-pink-800">
                     <h3 class="text-sm font-semibold truncate text-slate-500/90 dark:text-white/90
@@ -31,9 +46,5 @@
             <a href="{{ $url }}" target="_blank" rel="noopener noreferrer"
                class="text-xs text-slate-500 group-hover/preview:text-pink-600">From: {{ $shortUrl}}</a>
         </div>
-    </div>
-@elseif ($data->has('html'))
-    <div class="md:flex-shrink-0">
-        {!! $data->get('html') !!}
-    </div>
 @endif
+</div>
