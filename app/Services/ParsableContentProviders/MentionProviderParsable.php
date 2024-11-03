db2f8bcbcd3f55bff5b1c7c069ca2e8607747b8d
Punyapal Shah <53343069+MrPunyapal@users.noreply.github.com>
2024-11-04 00:21:20 +0530

    Update MentionProviderParsable.php

--- app/Services/ParsableContentProviders/MentionProviderParsable.php
+++ app/Services/ParsableContentProviders/MentionProviderParsable.php
@@ -14,7 +14,7 @@ final readonly class MentionProviderParsable implements ParsableContentProvider
     public function parse(string $content): string
     {
         return (string) preg_replace_callback(
-            '/(<(a|code|pre)\s+[^>]*>.*?<\/\2>)|@([a-z0-9_]+)/is',
+            '/(<(a|code|pre|div)\s+[^>]*>.*?<\/\2>)|@([a-z0-9_]+)/is',
             fn (array $matches): string => $matches[1] !== ''
                 ? $matches[1]
                 : '<a href="/@'.$matches[3].'" data-navigate-ignore="true" class="text-blue-500 hover:underline hover:text-blue-700 cursor-pointer" wire-navigate>@'.$matches[3].'</a>',
