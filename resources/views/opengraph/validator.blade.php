8d967a0d320d8a21d32673cd0773924b548238fb
Caen De Silva <caen@desilva.se>
2024-11-05 11:16:06 +0100

    Match aspect ratio and styles

--- resources/views/opengraph/validator.blade.php
+++ resources/views/opengraph/validator.blade.php
@@ -82,19 +82,22 @@ document.addEventListener('DOMContentLoaded', () => {
             const shortUrl = new URL(metadata.url).hostname;
 
             cardHtml = `
-                <a href="${metadata.url}" target="_blank" rel="noopener noreferrer">
-                    <div title="Click to visit: ${shortUrl}"
-                         class="relative w-full bg-slate-100/90 border border-slate-300 dark:border-0 rounded-lg overflow-hidden">
-                        <img src="${metadata.image}" alt="${title}" class="object-cover object-center w-full h-56" />
-                        <div class="absolute right-0 bottom-0 left-0 w-full rounded-b-lg bg-pink-100 bg-opacity-75 p-2 backdrop-blur-sm dark:bg-opacity-45 dark:bg-pink-800">
-                            <h3 class="text-sm font-semibold truncate text-slate-500/90 dark:text-white/90">${title}</h3>
+                <div id="link-preview-card" data-url="${metadata.url}" class="mx-auto mt-2 w-fit group/preview" data-navigate-ignore="true">
+                    <a href="${metadata.url}" target="_blank" rel="noopener noreferrer">
+                        <div title="Click to visit: ${shortUrl}" class="relative w-full bg-slate-100/90 border border-slate-300 
+                            dark:border-0 rounded-lg dark:group-hover/preview:border-0 overflow-hidden">
+                            <img src="${metadata.image}" alt="${title}" class="object-cover object-center w-full h-56">
+                            <div class="absolute right-0 bottom-0 left-0 w-full rounded-b-lg border-0 bg-pink-100 bg-opacity-75 p-2 backdrop-blur-sm backdrop-filter dark:bg-opacity-45 dark:bg-pink-800">
+                                <h3 class="text-sm font-semibold truncate text-slate-500/90 dark:text-white/90">
+                                    ${title}
+                                </h3>
+                            </div>
                         </div>
+                    </a>
+                    <div class="flex items-center justify-between pt-4">
+                        <a href="${metadata.url}" target="_blank" rel="noopener noreferrer" class="text-xs text-slate-500 group-hover/preview:text-pink-600">From: ${shortUrl}</a>
                     </div>
-                </a>
-                <div class="flex items-center justify-between pt-4">
-                    <a href="${metadata.url}" target="_blank" rel="noopener noreferrer" class="text-xs text-slate-500 dark:text-slate-500 dark:hover:text-pink-600 hover:text-pink-600">From: ${shortUrl}</a>
-                </div>
-            `;
+                </div>`;
         }
 
         previewCard.innerHTML = cardHtml;
