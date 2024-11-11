1cff82804df0ea559922bda79a730cfb3e89e90a
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-11 22:31:38 +1100

    fix: use the main layout for the other layout files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,43 +1,25 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    @include('layouts.components.head')
-</head>
-@php
-    $dotsImagePath = asset('/img/dots.svg');
-    $background = match($backgroundImage) {
-        'dots' => "dark:bg-slate-950 bg-slate-100",
-        'solid' => 'dark:bg-gray-900 bg-gray-100',
-        default => 'none',
-    };
-@endphp
-<body
-    class="{{ $background }} h-full antialiased slate-100 bg-center bg-repeat font-sans dark:text-slate-50 text-slate-950"
-    style="background-image: url({{ $backgroundImage === 'dots' ? $dotsImagePath : 'none' }})"
->
-@persist('flash-messages')
-<livewire:flash-messages.show/>
-@endpersist
+<x-main-layout backgroundImage="dots">
+    <div class="flex min-h-screen flex-col">
+        <main class="flex-grow">
+            <div class="fixed right-0 z-50">
+                @include('layouts.navigation')
+            </div>
 
-{{ $slot }}
+            <div>
+                <a
+                    href="{{ route('home.feed') }}"
+                    wire:navigate
+                    class="mt-20 flex justify-center"
+                >
+                    <x-pinkary-logo class="z-10 w-48" />
+                </a>
 
-@livewireScriptConfig
+                <div class="mx-auto w-full max-w-md px-4 py-10 sm:px-0">
+                    {{ $slot }}
+                </div>
+            </div>
+        </main>
 
-<script>
-    window.onload = function () {
-        const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-        if (timezone !== '{{ session()->get('timezone', 'UTC') }}') {
-            axios.post('{{ route('profile.timezone.update') }}', {timezone})
-        }
-
-        Livewire.hook('request', ({uri, options, payload, respond, succeed, fail}) => {
-            fail(({status, content, preventDefault}) => {
-                if (status === 419) {
-                    preventDefault()
-                }
-            })
-        })
-    }
-</script>
-</body>
-</html>
+        <x-footer />
+    </div>
+</x-main-layout>
