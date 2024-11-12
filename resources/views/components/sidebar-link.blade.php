639e25391986e55b4e33cabb3b803c654fbd0ef5
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-12 15:43:07 +1100

    feat(layout): extract sidebar link component

---
+++ resources/views/components/sidebar-link.blade.php
@@ -0,0 +1,6 @@
+<li {{ $attributes->merge(['class' => 'group']) }}>
+    <a href="{{ $href }}"
+       class="{{ $active ? 'bg-gray-800 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }} group w-full flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold">
+        {{ $slot }}
+    </a>
+</li>
