6a63896fcfc93c9b947842588d8a11fcb652870e
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-12 22:38:36 +1100

    feat(layout): adjust classes on textarea comp

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -3,7 +3,7 @@
     $componentId = $id ?? Str::uuid();
 @endphp
 <textarea
-    {!! $attributes->merge(['class' => 'w-full dark:text-white text-black dark:caret-white caret-black focus:border-pink-500 dark:border-slate-800 border-slate-300 dark:bg-slate-900/50 bg-slate-50/50 backdrop-blur-sm dark:focus:ring-slate-900 focus:ring-slate-100 rounded-lg shadow-sm sm:text-sm']) !!}
+    {!! $attributes->merge(['class' => 'w-full border dark:border-white/10 dark:bg-white/5 bg-slate-50/50 dark:text-white text-black dark:caret-white caret-black border-slate-300 rounded-lg shadow-sm text-sm shadow-sm transition py-1.5 focus:border-pink-500 focus:ring-4 focus:ring-pink-500/20']) !!}
     @if ($autocomplete === true)
         x-data="usesDynamicAutocomplete('{{ $componentId }}')"
         x-bind="autocompleteInputBindings"
