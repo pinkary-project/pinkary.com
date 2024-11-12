2421be5bb3db3cacc2e13f568dc63a05da09588e
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-12 23:00:14 +1100

    feat(layout): update the feed blade & livewire components

--- resources/views/home/feed.blade.php
+++ resources/views/home/feed.blade.php
@@ -1,13 +1,20 @@
-<x-feed-layout>
-    <div class="flex flex-col items-center justify-center">
-        <div class="w-full max-w-md overflow-hidden rounded-lg px-2 dark:shadow-md sm:px-0">
-            <x-home-menu></x-home-menu>
+<x-app-layout>
+    <header class="flex items-center justify-between border-b border-white/5 p-6 xl:px-8">
+        <h1 class="font-medium text-white text-base/7">Feed</h1>
 
-            @auth
-                <livewire:questions.create :toId="auth()->id()" />
-            @endauth
-
-            <livewire:home.feed />
+        <div class="text-sm space-x-1">
+            <a href="{{ route('home.trending') }}"
+               class="{{ request()->routeIs('home.trending') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800/30 text-gray-400' }} bg-gray-800 rounded-md px-2.5 py-1.5">Trending</a>
+            <a href="{{ route('home.following') }}"
+               class="{{ request()->routeIs('home.following') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800/30 text-gray-400' }} bg-gray-800 rounded-md px-2.5 py-1.5">Following</a>
+            <a href="{{ route('home.feed') }}"
+               class="{{ request()->routeIs('home.feed') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800/30 text-gray-400' }} bg-gray-800 rounded-md px-2.5 py-1.5">Recent</a>
         </div>
-    </div>
-</x-feed-layout>
+    </header>
+
+    @auth
+        <livewire:questions.create :toId="auth()->id()"/>
+    @endauth
+
+    <livewire:home.feed />
+</x-app-layout>
