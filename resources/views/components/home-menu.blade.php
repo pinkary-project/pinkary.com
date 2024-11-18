445c3ef7a2b89419ef5b62aa54e0529764726812
Nuno Guerra <mail@nunoguerra.com>
2024-11-18 00:48:34 +0000

    feat(layout): design tweaks on home feed menu

--- resources/views/components/home-menu.blade.php
+++ resources/views/components/home-menu.blade.php
@@ -3,30 +3,30 @@
         data-pan="home-tabs-trending"
         href="{{ route('home.trending') }}"
         title="{{ __('Trending') }}"
-        class="{{ request()->routeIs('home.trending') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800/30 text-gray-400' }} bg-gray-800 rounded-md px-2.5 py-1.5"
+        class="{{ request()->routeIs('home.trending') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800 text-gray-400' }} rounded-md px-2.5 py-1.5"
         wire:navigate
         wire:transition
     >
-        <span>{{ __('Trending') }}</span>
+        {{ __('Trending') }}
     </a>
     <a
         data-pan="home-tabs-following"
         href="{{ route('home.following') }}"
         title="{{ __('Following') }}"
-        class="{{ request()->routeIs('home.following') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800/30 text-gray-400' }} bg-gray-800 rounded-md px-2.5 py-1.5"
+        class="{{ request()->routeIs('home.following') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800 text-gray-400' }} rounded-md px-2.5 py-1.5"
         wire:navigate
         wire:transition
     >
-        <span>{{ __('Following') }}</span>
+        {{ __('Following') }}
     </a>
     <a
         data-pan="home-tabs-feed"
         href="{{ route('home.feed') }}"
         title="{{ __('Recent') }}"
-        class="{{ request()->routeIs('home.feed') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800/30 text-gray-400' }} bg-gray-800 rounded-md px-2.5 py-1.5"
+        class="{{ request()->routeIs('home.feed') ? 'bg-gray-800 text-white' : 'hover:bg-gray-800 text-gray-400' }} rounded-md px-2.5 py-1.5"
         wire:navigate
         wire:transition
     >
-        <span>{{ __('Recent') }}</span>
+        {{ __('Recent') }}
     </a>
 </div>
