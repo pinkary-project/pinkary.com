2dba35c6990c68ad0f65b9465b5c5931b626b1a5
Nuno Guerra <mail@nunoguerra.com>
2024-11-18 00:49:20 +0000

    feat(layout): tweak feed header design and make it sticky

--- resources/views/home/feed.blade.php
+++ resources/views/home/feed.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
-    <header class="flex items-center justify-between border-b border-white/5 p-6 xl:px-8">
-        <h1 class="font-medium text-white text-base/7">Feed</h1>
-        <x-home-menu />
+    <header class="sticky top-16 border-b border-white/5 bg-gray-900/50 backdrop-blur-lg">
+        <div class="flex items-center justify-between p-6 xl:px-8">
+            <h1 class="font-medium text-white text-base/7">Feed</h1>
+            <x-home-menu />
+        </div>
     </header>
 
     @auth
--- resources/views/home/trending-questions.blade.php
+++ resources/views/home/trending-questions.blade.php
@@ -1,11 +1,15 @@
 <x-app-layout>
-    <header class="flex items-center justify-between border-b border-white/5 p-6 xl:px-8">
-        <h1 class="font-medium text-white text-base/7">Feed</h1>
-        <x-home-menu />
+    <header class="sticky top-16 border-b border-white/5 bg-gray-900/50 backdrop-blur-lg">
+        <div class="flex items-center justify-between p-6 xl:px-8">
+            <h1 class="font-medium text-white text-base/7">Feed</h1>
+            <x-home-menu />
+        </div>
     </header>
 
     @auth
-        <livewire:questions.create :toId="auth()->id()"/>
+        <div class="border-b border-white/5">
+            <livewire:questions.create :toId="auth()->id()"/>
+        </div>
     @endauth
 
     <livewire:home.trending-questions :focus-input="true" />
