a7c197a1e6cc54477637cca52e5f042ed936fb2b
Punyapal Shah <mrpunyapal@gmail.com>
2025-04-23 22:11:47 +0530

    feat: implement profile switching functionality and update routes

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -29,6 +31,36 @@ final class FortifyServiceProvider extends ServiceProvider
         Fortify::ignoreRoutes();
         Fortify::loginView(fn () => view('auth.login'));
         Fortify::twoFactorChallengeView(fn () => view('auth.two-factor-challenge'));
+
+        Fortify::authenticateUsing(function (Request $request) {
+
+            $user = User::where('email', $request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                $accounts = session()->get('accounts', []);
+                $accounts = is_array($accounts) ? $accounts : [];
+
+                if (auth()->check()) {
+                    if ($user->id === auth()->id()) {
+                        return $user;
+                    }
+
+                    $accounts = array_unique([
+                        ...$accounts,
+                        auth()->id(),
+                    ]);
+                }
+
+                $accounts = array_unique([
+                    ...$accounts,
+                    $user->id,
+                ]);
+
+                session()->put('accounts', $accounts);
+
+                return $user;
+            }
+        });
 
         RateLimiter::for('login', function (Request $request) {
             $username = $request->string(Fortify::username())->toString();
