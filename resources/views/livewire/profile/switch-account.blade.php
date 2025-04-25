14b32a169fa0679bc6d49196e1acf19a1c0031f0
Punyapal Shah <mrpunyapal@gmail.com>
2025-04-25 21:38:51 +0530

    fix: disable dropdown button for the currently active account in switch-account view

--- resources/views/livewire/profile/switch-account.blade.php
+++ resources/views/livewire/profile/switch-account.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-1">
     @foreach ($accounts as $account)
         @if ($account->is(auth()->user()))
-            <x-dropdown-button class="dark:bg-slate-800 bg-slate-200">
+            <x-dropdown-button class="dark:bg-slate-800 bg-slate-200" disabled>
                 {{ '@' . $account->username }}
             </x-dropdown-button>
         @else
