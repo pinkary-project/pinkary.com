5ce45f42d9ec5c5b2c9d28263088ad3642429121
Tushar Nain <naintushar@hotmail.com>
2025-08-04 17:07:59 +0530

    feat: added weekly db vacuum command schedule

---
+++ app/Console/Commands/VacuumDatabaseCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+final class VacuumDatabaseCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:vacuum';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs VACUUM command on the database.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        DB::statement('VACUUM');
+    }
+}
