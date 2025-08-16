d0622e571411afd465fe501825550913312fe37d
Punyapal Shah <mrpunyapal@gmail.com>
2025-08-16 20:04:06 +0530

    chore: add Playwright for testing

---
+++ tests/Browser/AuthenticationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+test('user can register for a new account', function (): void {
+    $fixtureFile = base_path('tests/Fixtures/Turnstile/success.json');
+
+    $jsonFixture = File::json($fixtureFile);
+
+    Http::fake([
+        'https://challenges.cloudflare.com/turnstile/v0/siteverify' => Http::response($jsonFixture),
+    ]);
+
+    $page = visit('/register');
+
+    $page->assertPathContains('register')
+        ->assertSee('Register');
+
+    $page->type('name', 'John Doe')
+        ->type('username', 'johndoe')
+        ->type('email', 'john@example.com')
+        ->type('password', 'password')
+        ->type('password_confirmation', 'password')
+        ->check('#terms');
+
+    $page->script(<<<'EOT'
+        const terms = document.querySelector('#terms');
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = 'cf-turnstile-response';
+        input.value = 'valid-captcha-code';
+        terms.parentNode.insertBefore(input, terms.nextSibling);
+    EOT);
+
+    $page->submit();
+
+    $page->assertPathIs('/');
+});
