2ae9117b5292b285b2497323f8cda4472f1700e4
Punyapal Shah <mrpunyapal@gmail.com>
2026-03-31 00:53:45 +0530

    feat: refactor HomeController to utilize HomeFeedResponse for feed redirection

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Enums\UserDefaultFeed;
-use App\Models\User;
+use App\Http\Responses\HomeFeedResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -15,19 +14,8 @@ final readonly class HomeController
     /**
      * Display the home feed or redirect to the user's preferred feed on initial landing.
      */
-    public function __invoke(Request $request): View|RedirectResponse
+    public function __invoke(Request $request, HomeFeedResponse $response): View|RedirectResponse
     {
-        /** @var User|null $user */
-        $user = auth()->user();
-
-        if ($user instanceof User && ! $request->hasHeader('X-Livewire-Navigate')) {
-            return match ($user->default_feed) {
-                UserDefaultFeed::Following => redirect()->route('home.following'),
-                UserDefaultFeed::Trending => redirect()->route('home.trending'),
-                UserDefaultFeed::Recent => view('home/feed'),
-            };
-        }
-
-        return view('home/feed');
+        return $response->toResponse($request);
     }
 }
