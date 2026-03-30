14c3cf171919029fd7c09f5fa69f878999af0aae
Punyapal Shah <mrpunyapal@gmail.com>
2026-03-31 01:07:09 +0530

    feat: remove unused UserDefaultFeed import from navigation blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,4 @@
 @php
-    use App\Enums\UserDefaultFeed;
-
     $navClasses = 'fixed z-50 inset-0 md:inset-auto md:right-0 h-16 flex md:justify-end md:px-4 ';
     $navClasses .= auth()->check() ? ' justify-center' : ' justify-end px-4';
 
