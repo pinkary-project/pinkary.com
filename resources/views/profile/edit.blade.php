8318f4093ebb047807b5a7079958c4d053257c15
Punyapal Shah <mrpunyapal@gmail.com>
2026-05-16 00:32:22 +0530

    light mode in profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="border-b border-r border-slate-800/30 bg-[#07101f]/95 px-6 py-6">
+    <div class="border-b border-r border-slate-200/70 bg-white/80 px-6 py-6 dark:border-slate-800/30 dark:bg-[#07101f]/95">
         <div class="mx-auto max-w-2xl space-y-6">
             <div>
                 <a
@@ -12,37 +12,37 @@
                 </a>
             </div>
 
-            <div class="border border-slate-800/30 bg-[#0b1324] p-6">
+            <div class="border border-slate-200/70 bg-slate-50/80 p-6 dark:border-slate-800/30 dark:bg-[#0b1324]">
                 <div class="max-w-xl">
                     @include('profile.partials.verified-form')
                 </div>
             </div>
 
-            <div class="border border-slate-800/30 bg-[#0b1324] p-6">
+            <div class="border border-slate-200/70 bg-slate-50/80 p-6 dark:border-slate-800/30 dark:bg-[#0b1324]">
                 <div class="max-w-xl">
                     @include('profile.partials.upload-profile-photo-form')
                 </div>
             </div>
 
-            <div class="border border-slate-800/30 bg-[#0b1324] p-6">
+            <div class="border border-slate-200/70 bg-slate-50/80 p-6 dark:border-slate-800/30 dark:bg-[#0b1324]">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="border border-slate-800/30 bg-[#0b1324] p-6">
+            <div class="border border-slate-200/70 bg-slate-50/80 p-6 dark:border-slate-800/30 dark:bg-[#0b1324]">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="border border-slate-800/30 bg-[#0b1324] p-6">
+            <div class="border border-slate-200/70 bg-slate-50/80 p-6 dark:border-slate-800/30 dark:bg-[#0b1324]">
                 <div class="max-w-xl">
                     <livewire:profile.two-factor-authentication-form />
                 </div>
             </div>
 
-            <div class="border border-slate-800/30 bg-[#0b1324] p-6">
+            <div class="border border-slate-200/70 bg-slate-50/80 p-6 dark:border-slate-800/30 dark:bg-[#0b1324]">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
