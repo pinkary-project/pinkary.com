1d712c3beb1d9d78e0dde8a969b165fec9b5c2dc
Punyapal Shah <mrpunyapal@gmail.com>
2026-05-16 20:47:01 +0530

    style: update post-divider component for improved visual consistency

--- resources/views/components/post-divider.blade.php
+++ resources/views/components/post-divider.blade.php
@@ -4,14 +4,14 @@
 ])
 
 @if ($link !== null && $text !== null)
-    <div class="relative -my-2 flex min-h-12 items-center">
-        <span class="absolute inset-y-0 left-5 w-0.5 -translate-x-1/2 bg-slate-300 dark:bg-slate-600 sm:left-6" aria-hidden="true"></span>
+    <div class="relative -my-3 flex min-h-12 items-center">
+        <span class="absolute inset-y-0 left-5 w-0 -translate-x-1/2 border-l border-dotted border-slate-300 dark:border-slate-600 sm:left-6" aria-hidden="true"></span>
         <a href="{{ $link }}" class="ml-10 inline-flex items-center text-sm font-medium text-pink-500 transition-colors hover:text-pink-400 sm:ml-12">
             {{ $text }}
         </a>
     </div>
 @else
-    <div class="relative -my-2 h-8">
+    <div class="relative -my-3 h-100">
         <span class="absolute inset-y-0 left-5 w-0.5 -translate-x-1/2 bg-slate-300 dark:bg-slate-600 sm:left-6" aria-hidden="true"></span>
     </div>
 @endif
