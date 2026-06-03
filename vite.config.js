a79cf922a140bf0717a560f38dc2ae0f1996e97d
Punyapal Shah <mrpunyapal@gmail.com>
2026-06-03 15:24:18 +0530

    filament fix

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/filament/citadel/theme.css',
             ],
             refresh: true,
         }),
