04ef2bb1ccebe6600631b61b3ebf43fd31823b08
Punyapal Shah <mrpunyapal@gmail.com>
2026-06-16 17:54:44 +0530

    Fix: improve link actions visibility on mobile

--- resources/views/livewire/links/index.blade.php
+++ resources/views/livewire/links/index.blade.php
@@ -250,7 +250,7 @@
                             </div>
 
                             <div class="grow flex items-center justify-center transition-all duration-500"
-                                x-bind:class="{ '-translate-x-full': showActions }"
+                                x-bind:class="showActions ? 'max-sm:-translate-x-full sm:group-hover:-translate-x-full' : ''"
                             >
                                 <x-links.list-item :$user :$link />
                             </div>
@@ -266,8 +266,8 @@
                                 />
                             </div>
 
-                            <div class="absolute top-0 bottom-0 flex items-center justify-center transition-all duration-500 z-5"
-                                x-bind:class="showActions ? 'inset-0' : '-right-56'"
+                            <div class="absolute -right-56 top-0 bottom-0 flex items-center justify-center transition-all duration-500 z-5"
+                                x-bind:class="showActions ? 'max-sm:inset-0 sm:group-hover:inset-0' : ''"
                             >
                                 <div
                                     class="text-white min-w-fit cursor-help items-center gap-1 text-xs"
