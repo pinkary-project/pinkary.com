4bc493c97ea48b9c99b4ee7365e9966a6b40ebfa
Punyapal Shah <mrpunyapal@gmail.com>
2026-07-21 21:27:21 +0530

    revert

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->encryptCookies(except: [
-            '_home_redirected',
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
