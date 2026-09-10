d2dd076856580a5453927bdaa0d51b84ca79c919
Punyapal Shah <mrpunyapal@gmail.com>
2026-09-10 11:27:20 +0530

    fix notification z index

--- resources/views/livewire/flash-messages/show.blade.php
+++ resources/views/livewire/flash-messages/show.blade.php
@@ -21,7 +21,7 @@
     x-on:notification-created.dot.window="notify($event.detail.message, $event.detail.url, $event.detail.actionText)"
     @session('flash-message') x-init="notify('{{ $value }}')" @endsession
 >
-    <div id="flashMessageWrapper" class="fixed top-4 right-4 z-50 w-64 space-y-2"></div>
+    <div id="flashMessageWrapper" class="fixed top-4 right-4 z-120 w-64 space-y-2"></div>
 
     <template id="flashMessageTemplate">
         <div role="alert" class="mt-12 rounded-lg bg-pink-500 px-4 py-3 text-white">
