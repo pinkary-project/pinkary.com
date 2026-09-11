d89930e7e96bcd8de5238b8eb81a57f1d5b36482
Punyapal Shah <mrpunyapal@gmail.com>
2026-09-11 17:13:00 +0530

    Restore envelope and content tests

--- tests/Unit/Mail/PendingNotificationsTest.php
+++ tests/Unit/Mail/PendingNotificationsTest.php
@@ -9,6 +9,31 @@ use Spatie\MailcoachMailer\Exceptions\EmailNotValid;
 use Symfony\Component\HttpClient\Response\MockResponse;
 use Symfony\Component\Mailer\Exception\HttpTransportException;
 
+test('envelope', function (): void {
+    $user = User::factory()->create();
+
+    $mail = new PendingNotifications($user, 1);
+
+    $envelope = $mail->envelope();
+
+    expect($envelope->subject)
+        ->toBe('🌸 Pinkary: You Have 1 Notification! - '.now()->format('F j, Y'));
+});
+
+test('content', function (): void {
+    $user = User::factory()->create();
+
+    $mail = new PendingNotifications($user, 1);
+
+    foreach ([
+        '# Hello, '.$user->name.'!',
+        "We've noticed you have 1 notification. You can view notifications by clicking the button below.",
+        'If you no longer wish to receive these emails, you can change your "Mail Preference Time" in your [profile settings]('.config('app.url').'/profile).',
+    ] as $line) {
+        $mail->assertSeeInText($line);
+    }
+});
+
 test('records suppression on mailcoach rejection', function (): void {
     $user = User::factory()->create();
 
