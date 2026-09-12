d8d0a80a6df2194d50a6f8a10cc8521be884fb57
Sangram <okclsangram@gmail.com>
2026-09-12 11:13:16 +0530

    fix light mode checkbox visibility (#877)
    * feat: implement theme toggle component with system, light, and dark mode support
    
    * feat: add registration and login views with custom checkbox component and updated Tailwind CSS configuration
    
    * Revert "feat: implement theme toggle component with system, light, and dark mode support"
    
    This reverts commit cc9f7e03d4b51fe1c4c71f4456bdab9b5550f738.
    
    * refactor: centralize checkbox checked styles
    
    ---------
    
    Co-authored-by: Sangram <apexsangram@gmailcom>

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,4 +1,4 @@
 <input
     type="checkbox"
-    {!! $attributes->merge(['class' => 'focus:ring-pink-500 dark:focus:ring-offset-slate-900 focus:ring-offset-slate-200 size-4.5 text-pink-500 dark:border-slate-800 border-slate-200 dark:bg-slate-900 bg-slate-50 dark:ring-slate-900 ring-slate-200 rounded cursor-pointer dark:shadow-none shadow-md']) !!}
+    {!! $attributes->merge(['class' => 'focus:ring-pink-500 dark:focus:ring-offset-slate-900 focus:ring-offset-slate-200 size-4.5 text-pink-500 dark:border-slate-800 border-slate-200 dark:bg-slate-900 bg-slate-50 checked:bg-pink-500 dark:checked:bg-pink-500 checked:border-pink-500 dark:checked:border-pink-500 dark:ring-slate-900 ring-slate-200 rounded cursor-pointer dark:shadow-none shadow-md']) !!}
 />
